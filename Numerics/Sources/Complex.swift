62caae648c5246847bfcad8ef6112d0512188134
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-05-16 00:58:00 +0900

    disable lapack types

--- Numerics/Sources/Complex.swift
+++ Numerics/Sources/Complex.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Kota on 5/15/R7.
 //
-import simd
 public protocol ComplexNumber<FloatLiteralType>: Numeric & Comparable & Hashable & CustomStringConvertible & Sendable & Copyable where IntegerLiteralType == FloatLiteralType.IntegerLiteralType, Magnitude == FloatLiteralType {
 	associatedtype FloatLiteralType: SignedNumeric & Comparable & Hashable
 	var real: FloatLiteralType { get }
@@ -74,55 +73,5 @@ extension ComplexNumber {
 extension ComplexNumber where FloatLiteralType: ComplexNumber {
 	public var simplified: FloatLiteralType {
 		real + imag * .i
-	}
-}
-extension ComplexNumber where FloatLiteralType: SIMDScalar & BinaryFloatingPoint {
-	public static func+(lhs: Self, rhs: Self) -> Self {
-		unsafeBitCast(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self) + unsafeBitCast(rhs, to: SIMD2<FloatLiteralType>.self), to: Self.self)
-	}
-	public static func-(lhs: Self, rhs: Self) -> Self {
-		unsafeBitCast(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self) - unsafeBitCast(rhs, to: SIMD2<FloatLiteralType>.self), to: Self.self)
-	}
-}
-extension ComplexNumber where FloatLiteralType == Float32 {
-	public init(r: FloatLiteralType, θ: FloatLiteralType) {
-		let θ = r * unsafeBitCast(__sincosf_stret(θ), to: SIMD2<FloatLiteralType>.self)
-		self.init(real: θ.y, imag: θ.x)
-	}
-	public static func*(lhs: Self, rhs: Self) -> Self {
-		unsafeBitCast(SIMD2(
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(rhs.real, -rhs.imag)),
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(rhs.imag,  rhs.real))
-		), to: Self.self)
-	}
-	public static func/(lhs: Self, rhs: Self) -> Self {
-		unsafeBitCast(SIMD2(
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2( rhs.real, rhs.imag)),
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(-rhs.imag, rhs.real))
-		) / length_squared(unsafeBitCast(rhs, to: SIMD2<FloatLiteralType>.self)), to: Self.self)
-	}
-	public var magnitude: FloatLiteralType {
-		length(unsafeBitCast(self, to: SIMD2<FloatLiteralType>.self))
-	}
-}
-extension ComplexNumber where FloatLiteralType == Float64 {
-	public init(r: FloatLiteralType, θ: FloatLiteralType) {
-		let θ = r * unsafeBitCast(__sincos_stret(θ), to: SIMD2<FloatLiteralType>.self)
-		self.init(real: θ.y, imag: θ.x)
-	}
-	public static func*(lhs: Self, rhs: Self) -> Self {
-		unsafeBitCast(SIMD2(
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(rhs.real, -rhs.imag)),
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(rhs.imag,  rhs.real))
-		), to: Self.self)
-	}
-	public static func/(lhs: Self, rhs: Self) -> Self {
-		unsafeBitCast(SIMD2(
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2( rhs.real, rhs.imag)),
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(-rhs.imag, rhs.real))
-		) / rhs.magnitude, to: Self.self)
-	}
-	public var magnitude: FloatLiteralType {
-		length(unsafeBitCast(self, to: SIMD2<FloatLiteralType>.self))
 	}
 }
