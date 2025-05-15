7e7d042934ce652724d4b6c8f8eaa1a630366dea
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-05-16 02:11:09 +0900

    rename

---
+++ Numerics/Sources/Quoternion.swift
@@ -0,0 +1,20 @@
+//
+//  Quoternion.swift
+//  MUSE
+//
+//  Created by Kota on 5/15/R7.
+//
+public protocol QuoternionNumber: ExpressibleByIntegerLiteral, ExpressibleByFloatLiteral where FloatLiteralType: BinaryFloatingPoint & SIMDScalar {
+	var real: FloatLiteralType { get set }
+	var imag: SIMD3<FloatLiteralType> { get set }
+	init(real: FloatLiteralType, imag: SIMD3<FloatLiteralType>)
+	init(ix: FloatLiteralType, iy: FloatLiteralType, iz: FloatLiteralType, r: FloatLiteralType)
+}
+extension QuoternionNumber {
+	public init(floatLiteral value: FloatLiteralType) {
+		self.init(real: .init(value), imag: .zero)
+	}
+	public init(integerLiteral value: FloatLiteralType.IntegerLiteralType) {
+		self.init(floatLiteral: .init(integerLiteral: value))
+	}
+}
