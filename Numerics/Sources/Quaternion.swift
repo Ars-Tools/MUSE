285dc7ce73231e348b172dfea2222215af074393
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-05-16 00:47:10 +0900

    quoternion

---
+++ Numerics/Sources/Quaternion.swift
@@ -0,0 +1,12 @@
+//
+//  Quoternion.swift
+//  MUSE
+//
+//  Created by Kota on 5/15/R7.
+//
+import typealias simd.simd_quath
+import typealias simd.simd_quatf
+import typealias simd.simd_quatd
+public typealias Quoternion64 = simd_quath
+public typealias Quoternion128 = simd_quatf
+public typealias Quoternion256 = simd_quatd
