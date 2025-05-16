91afcc23b2dac85b7244b28063397e4a0e882cd9
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-05-16 16:04:12 +0900

    disable rangeexpression for Int

--- Layout/Sources/Axis.swift
+++ Layout/Sources/Axis.swift
@@ -4,15 +4,15 @@
 //
 //  Created by Kota on 5/16/R7.
 //
-extension Int: @retroactive RangeExpression {
-	@_disfavoredOverload
-	@inlinable @inline(__always)
-	public func relative<C>(to collection: C) -> Range<Int> where C : Collection, Int == C.Index {
-		(self..<self).relative(to: collection)
-	}
-	@_disfavoredOverload
-	@inlinable @inline(__always)
-	public func contains(_ element: Int) -> Bool {
-		(self..<self).contains(element)
-	}
-}
+//extension Int: @retroactive RangeExpression {
+//	@_disfavoredOverload
+//	@inlinable@inline(__always)
+//	public func relative<C>(to collection: C) -> Range<Int> where C : Collection, Int == C.Index {
+//		(self...self).relative(to: collection)
+//	}
+//	@_disfavoredOverload
+//	@inlinable@inline(__always)
+//	public func contains(_ element: Int) -> Bool {
+//		(self...self).contains(element)
+//	}
+//}
---
+++ Layout/Tests/Axis.swift
@@ -0,0 +1,20 @@
+//
+//  Axis.swift
+//  MUSE
+//
+//  Created by Kota on 5/16/R7.
+//
+//import Testing
+//@testable import Layout
+//@Suite
+//struct AxisTests {
+//	@Test
+//	func `switch`() {
+//		switch 0 {
+//		case 0:
+//			break
+//		default:
+//			Issue.record()
+//		}
+//	}
+//}
