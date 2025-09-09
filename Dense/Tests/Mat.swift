b0770b410ccaab98bfaebdef8bf1e924638ce5ce
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-09 10:37:00 +0900

    add(ing) tests

---
+++ Dense/Tests/Mat.swift
@@ -0,0 +1,19 @@
+//
+//  Mat.swift
+//  MUSE
+//
+//  Created by Kota on 9/9/R7.
+//
+import Testing
+@testable import Dense
+@Suite
+struct MatrixTestCases {
+	@Test
+	func arrayLiteral() {
+		let m = MatBuf(rows: 4, cols: 4, ldr: 4, ldc: 1, data: .init(0..<16))
+		#expect(m[0, 0] ==  0)
+		#expect(m[0, 3] ==  3)
+		#expect(m[3, 0] == 12)
+		#expect(m[3, 3] == 15)
+	}
+}
