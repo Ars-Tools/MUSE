010138dec94c08825c895790ad121d1df330c084
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-09 11:10:40 +0900

    add test cases

--- Dense/Tests/Mat.swift
+++ Dense/Tests/Mat.swift
@@ -9,11 +9,20 @@ import Testing
 @Suite
 struct MatrixTestCases {
 	@Test
-	func arrayLiteral() {
+	func new() {
 		let m = MatBuf(rows: 4, cols: 4, ldr: 4, ldc: 1, data: .init(0..<16))
 		#expect(m[0, 0] ==  0)
 		#expect(m[0, 3] ==  3)
 		#expect(m[3, 0] == 12)
 		#expect(m[3, 3] == 15)
+	}
+	@Test
+	func arrayLiteral() {
+		let m = MatBuf(cols: [
+			[0, 1, 2],
+			[3, 4, 5, 6],
+			[7, 8, 9]
+		])
+		print(m)
 	}
 }
