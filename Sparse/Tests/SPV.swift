9bef27bd3ed886b2ac8095f5f40a52d69f6857d7
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-10 12:24:47 +0900

    vector test

---
+++ Sparse/Tests/SPV.swift
@@ -0,0 +1,20 @@
+//
+//  SPV.swift
+//  MUSE
+//
+//  Created by Kota on 9/9/R7.
+//
+import Testing
+@testable import Sparse
+@Suite
+struct SPVTestCases {
+	@Test
+	func create() {
+		let v = [0, 1, 0, 2, 0, 3] as SPV
+		#expect(v.store.count == 3)
+		#expect(v[0] == 0)
+		#expect(v[1] == 1)
+		#expect(v[3] == 2)
+		#expect(v[5] == 3)
+	}
+}
