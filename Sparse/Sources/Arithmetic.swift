ebe0a362bb1cf8307a6e969cf62b02f9c2e397b0
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-11 19:15:29 +0900

    prepare for broadcasting

--- Sparse/Sources/Arithmetic.swift
+++ Sparse/Sources/Arithmetic.swift
@@ -2,5 +2,28 @@
 //  Arithmetic.swift
 //  MUSE
 //
-//  Created by Kota on 9/10/R7.
+//  Created by Kota on 9/11/R7.
 //
+import typealias Layout.MemoryStrategy
+@inlinable@inline(__always)
+func`repeat`<Λ, Element>(lil: Λ, count: (Int, Int)) -> LazySequence<FlattenSequence<LazyMapSequence<Repeated<LazyMapSequence<Λ, Dictionary<Int, Element>>>, LazyMapSequence<Λ, Dictionary<Int, Element>>>>> where Λ: Collection, Λ.Element: Sequence, Λ.Element.Element == (Int, Element) {
+	repeatElement(lil.lazy.map {
+		switch count.1 {
+		case 1:
+			Dictionary(uniqueKeysWithValues: $0)
+		case let count:
+			Dictionary(uniqueKeysWithValues: $0.lazy.flatMap { repeatElement($1, count: count).enumerated() })
+		}
+	}, count: count.0).lazy.flatMap(\.self)
+}
+@inlinable@inline(__always)
+func`repeat`<Λ, Element>(lil: Λ, count: (Int, Int)) -> LazySequence<FlattenSequence<LazyMapSequence<Repeated<LazyMapSequence<Λ, Dictionary<Int, Element>>>, LazyMapSequence<Λ, Dictionary<Int, Element>>>>> where Λ: Collection, Λ.Element == Dictionary<Int, Element> {
+	repeatElement(lil.lazy.map {
+		switch count.1 {
+		case 1:
+			$0
+		case let count:
+			Dictionary(uniqueKeysWithValues: $0.lazy.flatMap { repeatElement($1, count: count).enumerated() })
+		}
+	}, count: count.0).lazy.flatMap(\.self)
+}
