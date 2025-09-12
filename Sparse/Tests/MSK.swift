872a0c4fe29344d5b76d72f1f6c0deb7728306e6
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-12 10:38:23 +0900

    boolean storage

---
+++ Sparse/Tests/MSK.swift
@@ -0,0 +1,37 @@
+//
+//  MSK.swift
+//  MUSE
+//
+//  Created by Kota on 9/10/R7.
+//
+import Testing
+@testable import Sparse
+@Suite
+struct MSKTestCases {
+	@Test
+	func create() {
+		var m = MSK(shape: (3, 3))
+		#expect(m.isEmpty)
+		m[0, 0] = true
+		m[0, 1] = true
+		#expect(m.state.count == 2)
+		m[0, 0] = false
+		m[0, 1] = true
+		#expect(m.state.count == 1)
+	}
+	@Test
+	func convert() {
+		let ccs = CCS(cols: [
+			[0.0, .ulpOfOne],
+			[0.0, .nan]
+		])
+		#expect(ccs[1, 1] == .zero)
+		#expect(ccs[1, 0] != .zero)
+	}
+	@Test
+	func identity() {
+		let s = MSK(identity: 10)
+		print(s)
+	}
+}
+
