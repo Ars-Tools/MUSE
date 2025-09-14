987e264d9cb2243b1c131415b1d542a307619477
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-15 00:43:16 +0900

    basical comutation

--- Sparse/Sources/Arithmetic.swift
+++ Sparse/Sources/Arithmetic.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Kota on 9/11/R7.
 //
+import typealias Foundation.KeyPathComparator
 import protocol Accelerate.AccelerateBuffer
 import protocol Dense.Vector
 import typealias Layout.MemoryStrategy
@@ -33,8 +34,8 @@ extension Arithmetic.ANY: SparseVector {
 		.init(core: core[bounds])
 	}
 	@usableFromInline
-	var coo: AnySequence<(Int, Element)> {
-		.init((core.coo as any Sequence<(Int, Element)>))
+	var coo: some Sequence<(Int, Element)> {
+		AnySequence<(Int, Element)>((core.coo as any Sequence<(Int, Element)>))
 	}
 }
 extension Arithmetic {
@@ -73,7 +74,7 @@ extension Arithmetic.Scale.Vector: SparseVector {
 	}
 	@inlinable@inline(__always)
 	var coo: LazyMapSequence<Source.COO, (Int, Element)> {
-		source.coo.lazy.map { [factor] in ($0, $1 * factor) }
+		factor * source.coo
 	}
 }
 extension Arithmetic.Scale.Matrix: SparseMatrix {
@@ -109,7 +110,7 @@ extension Arithmetic.Scale.Matrix: SparseMatrix {
 	func lil(for layout: MemoryStrategy) -> (MemoryStrategy, LazyMapSequence<Source.LIL, LazyMapSequence<Source.LIL.Element, (Int, Element)>>) {
 		switch source.lil(for: layout) {
 		case (let layout, let source):
-			(layout, source.lazy.map { [factor] in $0.lazy.map { ($0, $1 * factor) } })
+			(layout, source.lazy.map { [factor] in factor * $0 } )
 		}
 	}
 }
@@ -118,4 +119,199 @@ public func*<Element: Numeric>(lhs: Element, rhs: some SparseVector<Element>) ->
 }
 public func*<Element: Numeric>(lhs: Element, rhs: some SparseMatrix<Element>) -> some SparseMatrix<Element> {
 	Arithmetic.Scale.Matrix(factor: lhs, source: rhs)
+}
+// MARK: Scale
+@inlinable@inline(__always)
+func*<Element: Numeric, Source: Sequence<(Int, Element)>>(_ factor: Element, _ source: Source) -> LazyMapSequence<Source, (Int, Element)> {
+	source.lazy.map { ($0, factor * $1) }
+}
+@_disfavoredOverload
+@inlinable@inline(__always)
+func*<Element: Numeric>(_ factor: Element, _ source: Dictionary<Int, Element>) -> some Sequence<(Int, Element)> {
+	source.lazy.map { ($0, factor * $1) }
+}
+// MARK: ADD
+@inlinable@inline(__always)
+func+<Element: Numeric>(_ lhs: some Sequence<(Int, Element)>, _ rhs: some Sequence<(Int, Element)>) -> some Sequence<(Int, Element)> {
+	var lhs = lhs.sorted(using: KeyPathComparator(\.0)).makeIterator()
+	var rhs = rhs.sorted(using: KeyPathComparator(\.0)).makeIterator()
+	return sequence(state: (lhs.next(), rhs.next())) {
+		switch $0 {
+		case (.some(let x), .some(let y)) where x.0 < y.0:
+			fallthrough
+		case (.some(let x), .none):
+			$0.0 = lhs.next()
+			return.some(x)
+		case (.some(let x), .some(let y)) where x.0 > y.0:
+			fallthrough
+		case (.none, .some(let y)):
+			$0.1 = rhs.next()
+			return.some(y)
+		case (.some(let x), .some(let y)):
+			assert(x.0 == y.0)
+			$0 = (lhs.next(), rhs.next())
+			return.some((min(x.0, y.0), x.1 + y.1))
+		case (.none, .none):
+			return.none
+		}
+	}
+}
+@_disfavoredOverload
+@inlinable@inline(__always)
+func+<Element: Numeric>(lhs: some Sequence<(Int, Element)>, rhs: Dictionary<Int, Element>) -> some Sequence<(Int, Element)> {
+	let rhs: some Sequence<(Int, Element)> = rhs.lazy.map(\.self)
+	return lhs + rhs
+}
+@_disfavoredOverload
+@inlinable@inline(__always)
+func+<Element: Numeric>(lhs: Dictionary<Int, Element>, rhs: some Sequence<(Int, Element)>) -> some Sequence<(Int, Element)> {
+	let lhs: some Sequence<(Int, Element)> = lhs.lazy.map(\.self)
+	return lhs + rhs
+}
+@_disfavoredOverload
+@inlinable@inline(__always)
+func+<Element: Numeric>(lhs: Dictionary<Int, Element>, rhs: Dictionary<Int, Element>) -> LazyMapSequence<LazyFilterSequence<LazyMapSequence<Set<Int>, Optional<(Int, Element)>>>, (Int, Element)> {
+	let key = switch (lhs.keys, rhs.keys) {
+	case (let l, let r) where l.count < r.count:
+		Set(r).union(l)
+	case (let l, let r):
+		Set(l).union(r)
+	}
+	return key.lazy.compactMap {
+		switch lhs[$0, default: .zero] + rhs[$0, default: .zero] {
+		case.zero:
+			.none
+		case let v:
+			.some(($0, v))
+		}
+	}
+}
+// MARK: SUB
+@inlinable@inline(__always)
+func-<Element: Numeric>(_ lhs: some Sequence<(Int, Element)>, _ rhs: some Sequence<(Int, Element)>) -> some Sequence<(Int, Element)> {
+	var lhs = lhs.sorted(using: KeyPathComparator(\.0)).makeIterator()
+	var rhs = rhs.sorted(using: KeyPathComparator(\.0)).makeIterator()
+	return sequence(state: (lhs.next(), rhs.next())) {
+		switch $0 {
+		case (.some(let x), .some(let y)) where x.0 < y.0:
+			$0.0 = lhs.next()
+			return.some(x)
+		case (.some(let x), .some(let y)) where x.0 > y.0:
+			$0.1 = rhs.next()
+			return.some((y.0, .zero - y.1))
+		case (.some(let x), .some(let y)):
+			assert(x.0 == y.0)
+			$0 = (lhs.next(), rhs.next())
+			return.some((min(x.0, y.0), x.1 + y.1))
+		case (.some(let x), .none):
+			return.some(x)
+		case (.none, .some(let y)):
+			return.some(y)
+		case (.none, .none):
+			return.none
+		}
+	}
+}
+@_disfavoredOverload
+@inlinable@inline(__always)
+func-<Element: Numeric>(lhs: some Sequence<(Int, Element)>, rhs: Dictionary<Int, Element>) -> some Sequence<(Int, Element)> {
+	let rhs: some Sequence<(Int, Element)> = rhs.lazy.map(\.self)
+	return lhs - rhs
+}
+@_disfavoredOverload
+@inlinable@inline(__always)
+func-<Element: Numeric>(lhs: Dictionary<Int, Element>, rhs: some Sequence<(Int, Element)>) -> some Sequence<(Int, Element)> {
+	let lhs: some Sequence<(Int, Element)> = lhs.lazy.map(\.self)
+	return lhs - rhs
+}
+@_disfavoredOverload
+@inlinable@inline(__always)
+func-<Element: Numeric>(lhs: Dictionary<Int, Element>, rhs: Dictionary<Int, Element>) -> LazyMapSequence<LazyFilterSequence<LazyMapSequence<Set<Int>, Optional<(Int, Element)>>>, (Int, Element)> {
+	let key = switch (lhs.keys, rhs.keys) {
+	case (let l, let r) where l.count < r.count:
+		Set(r).union(l)
+	case (let l, let r):
+		Set(l).union(r)
+	}
+	return key.lazy.compactMap {
+		switch lhs[$0, default: .zero] - rhs[$0, default: .zero] {
+		case.zero:
+			.none
+		case let v:
+			.some(($0, v))
+		}
+	}
+}
+// MARK: MUL
+@inlinable@inline(__always)
+func*<Element: Numeric>(_ lhs: some Sequence<(Int, Element)>, _ rhs: some Sequence<(Int, Element)>) -> some Sequence<(Int, Element)> {
+	var lhs = lhs.sorted(using: KeyPathComparator(\.0)).makeIterator()
+	var rhs = rhs.sorted(using: KeyPathComparator(\.0)).makeIterator()
+	return sequence(state: (lhs.next(), rhs.next())) {
+		while let x = $0.0 ?? lhs.next(), let y = $0.0 ?? rhs.next() {
+			if x.0 == y.0 {
+				return.some((min(x.0, y.0), x.1 * y.1))
+			} else if x.0 < y.0 {
+				$0.0 = lhs.next()
+			} else if x.0 > y.0 {
+				$0.1 = rhs.next()
+			} else {
+				assertionFailure()
+				break
+			}
+		}
+		return.none
+	}
+}
+@_disfavoredOverload
+@inlinable@inline(__always)
+func*<Element: Numeric>(lhs: some Sequence<(Int, Element)>, rhs: Dictionary<Int, Element>) -> some Sequence<(Int, Element)> {
+	let rhs: some Sequence<(Int, Element)> = rhs.lazy.map(\.self)
+	return lhs * rhs
+}
+@_disfavoredOverload
+@inlinable@inline(__always)
+func*<Element: Numeric>(lhs: Dictionary<Int, Element>, rhs: some Sequence<(Int, Element)>) -> some Sequence<(Int, Element)> {
+	let lhs: some Sequence<(Int, Element)> = lhs.lazy.map(\.self)
+	return lhs * rhs
+}
+@_disfavoredOverload
+@inlinable@inline(__always)
+func*<Element: Numeric>(lhs: Dictionary<Int, Element>, rhs: Dictionary<Int, Element>) -> LazyMapSequence<LazyFilterSequence<LazyMapSequence<Set<Int>, Optional<(Int, Element)>>>, (Int, Element)> {
+	let key = switch (lhs.keys, rhs.keys) {
+	case (let l, let r) where l.count < r.count:
+		Set(r).intersection(l)
+	case (let l, let r):
+		Set(l).intersection(r)
+	}
+	return key.lazy.compactMap {
+		switch lhs[$0, default: .zero] * rhs[$0, default: .zero] {
+		case.zero:
+			.none
+		case let v:
+			.some(($0, v))
+		}
+	}
+}
+// MARK: DOT
+@inlinable@inline(__always)
+func •<Element: Numeric>(_ lhs: some Sequence<(Int, Element)>, _ rhs: some Sequence<(Int, Element)>) -> Element {
+	var lhs = lhs.sorted(using: KeyPathComparator(\.0)).makeIterator()
+	var rhs = rhs.sorted(using: KeyPathComparator(\.0)).makeIterator()
+	var l = lhs.next()
+	var r = rhs.next()
+	var a = Element.zero
+	while let x = l ?? lhs.next(), let y = r ?? rhs.next() {
+		if x.0 == y.0 {
+			a += x.1 * y.1
+		} else if x.0 < y.0 {
+			l = lhs.next()
+		} else if x.0 > y.0 {
+			r = rhs.next()
+		} else {
+			assertionFailure()
+			break
+		}
+	}
+	return a
 }
