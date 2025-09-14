2b9b4f53dbd1211c11827af94b3d5f296951b7af
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-15 00:45:03 +0900

    QR decomposition

---
+++ Sparse/Sources/LinAlg+QR.swift
@@ -0,0 +1,40 @@
+//
+//  LinAlg+QR.swift
+//  MUSE
+//
+//  Created by Kota on 9/13/R7.
+//
+import protocol Dense.Matrix
+import func simd.sqrt
+import func simd.copysign
+extension LinAlg {
+	public static func QR(_ a: some SparseMatrix<Element>, ε: Element.Magnitude = .ulpOfOne) -> (some SparseMatrix<Element>, some SparseMatrix<Element>){
+		precondition(a.rows <= a.cols)
+		var q = LIL<Element>(identity: a.rows, for: .rowMajor)
+		var r = LIL(a, for: .columnMajor)
+		for col in r.store.indices.dropLast() {
+			assert(q.major == .rowMajor)
+			assert(r.major == .columnMajor)
+			var u = r[col..., col]
+			switch u[0].sign {
+			case.plus:
+				u[0] += norm(u)
+			case.minus:
+				u[0] -= norm(u)
+			}
+			switch normSquared(u) {
+			case ...(.ulpOfOne):
+				continue
+			case let n:assert(.ulpOfOne < n)
+				var h = LIL<Element>(major: .columnMajor, store: .init(repeating: .init(), count: r.count), count: r.count)
+				let p = LIL<Element>(identity: u.count, for: .columnMajor) - (2 / n) * outer(u, u)
+				h[0..<col, 0..<col] = .init(identity: col, for: .columnMajor)
+				h[col..., col...] = .init(p)
+				q = LIL(q • h.transpose, for: .rowMajor)
+				r = LIL(h • r, for: .columnMajor)
+				r[(col+1)..., col] = .init(shape: u.count, [])
+			}
+		}
+		return (q, r)
+	}
+}
