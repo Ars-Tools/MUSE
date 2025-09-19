d7f4537288210ae0db2904b6e87b42a0e2607d41
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-19 15:51:28 +0900

    refactor

--- Dense/Sources/Protocol+Tensor.swift
+++ Dense/Sources/Protocol+Tensor.swift
@@ -7,13 +7,15 @@
 import protocol Accelerate.AccelerateBuffer
 import protocol Accelerate.AccelerateMutableBuffer
 import typealias Layout.MemoryStrategy
+public typealias Storage = Sendable & RandomAccessCollection & AccelerateBuffer
+public typealias MutableStorage = Storage & MutableCollection & AccelerateMutableBuffer
 public protocol Tensor<Element>: Sendable {
-	associatedtype Element: BitwiseCopyable & Hashable & Sendable
+	associatedtype Element: BitwiseCopyable & Sendable
 	associatedtype S: Tensor<Element>
 	associatedtype T: Tensor<Element>
-	associatedtype U: Tensor<Element>
-	associatedtype V: Tensor<Element>
-	associatedtype R: AccelerateBuffer<Element>
+	associatedtype U: Scalar<Element>
+	associatedtype V: Vector<Element>
+	associatedtype R: Storage where R.Element == Element, R.Index: Strideable, R.Index.Stride == Int, R.SubSequence: Storage
 	@inlinable var shape: Array<Int> { get }
 	@inlinable var transpose: T { get }
 	@inlinable var diagonal: V { get }
@@ -21,8 +23,20 @@ public protocol Tensor<Element>: Sendable {
 	@inlinable subscript<Q: RandomAccessCollection>(bounds: Q) -> S where Q.Index == Int, Q.Element: RangeExpression<Int> { get }
 	@inlinable func callAsFunction(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> R)
 }
-public protocol MutTensor<Element>: Tensor where U == Element, R: AccelerateMutableBuffer {
+public protocol MutTensor<Element>: Tensor where S: MutTensor<Element>, T: MutTensor<Element>, U: MutScalar<Element>, V: MutVector<Element>, R: MutableStorage, R.SubSequence: MutableStorage {
 	@inlinable subscript<P: RandomAccessCollection>(position: P) -> U where P.Index == Int, P.Element == Int { get set }
 	@inlinable subscript<Q: RandomAccessCollection>(bounds: Q) -> S where Q.Index == Int, Q.Element: RangeExpression<Int> { get set }
+}
+public protocol Immediate<Element>: Tensor {
+	@inlinable func callAsFunction(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> R)
+}
+extension Immediate {
+	@inlinable
+	public func callAsFunction(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> R) {
+		switch try callAsFunction(for: strategy) as (Array<Int>, @Sendable () -> R) {
+		case (let stride, let kernel):
+			(stride, kernel)
+		}
+	}
 }
 infix operator •: MultiplicationPrecedence
