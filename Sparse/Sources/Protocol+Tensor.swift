76cbed8dcb57a91b3f02fe7fd0745f6f2f8cecef
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-19 16:03:51 +0900

    remove tensor

--- Sparse/Sources/Protocol+Tensor.swift
+++ Sparse/Sources/Protocol+Tensor.swift
@@ -10,36 +10,40 @@ import protocol Dense.Tensor
 import protocol Dense.MutTensor
 import protocol Dense.MutScalar
 public typealias SparseScalar<Element> = MutScalar<Element>
-public protocol SparseTensor<Element>: Tensor where R == Array<Element>, S: SparseTensor, T: SparseTensor, U: SparseScalar<Element>, V: SparseTensor {
-	associatedtype Nonzero: Sequence where Nonzero.Element == (Array<Int>, Element)
-	@inlinable var nonzero: Nonzero { get }
+public typealias MutSparseScalar<Element> = SparseScalar<Element>
+public protocol SparseTensor<Element>: Tensor where S: SparseTensor<Element>, T: SparseTensor<Element>, U: SparseScalar<Element>, V: SparseVector<Element>, R == Array<Element> {
 }
-public protocol MutSparseTensor<Element>: MutTensor & SparseTensor {}
-extension SparseTensor where Element: Numeric {
-	@inlinable
-	public func callAsFunction(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
-		let layout = strategy.stride(for: shape)
-		let memory = Array<Element>(unsafeUninitializedCapacity: capacity(alloc: shape, stride: layout)) {
-			$0.initialize(repeating: .zero)
-			for (key, value) in nonzero {
-				$0[zip(key, layout).lazy.map(*).reduce(0, +)] = value
-			}
-			$1 = $0.count
-		}
-		return (layout, {memory})
-	}
-}
-extension SparseTensor where Element == Bool {
-	@inlinable
-	public func callAsFunction(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
-		let layout = strategy.stride(for: shape)
-		let memory = Array<Element>(unsafeUninitializedCapacity: capacity(alloc: shape, stride: layout)) {
-			$0.initialize(repeating: false)
-			for (key, value) in nonzero {
-				$0[zip(key, layout).lazy.map(*).reduce(0, +)] = value
-			}
-			$1 = $0.count
-		}
-		return (layout, {memory})
-	}
-}
+public protocol MutSparseTensor<Element>: MutTensor & SparseTensor where S: MutSparseTensor<Element>, T: MutSparseTensor<Element>, U: MutSparseScalar<Element>, V: MutSparseVector<Element> {}
+//public protocol SparseTensor<Element>: Tensor where R == Array<Element>, S: SparseTensor<Element>, T: SparseTensor<Element>, U: SparseScalar<Element>, V: SparseVector<Element> {
+//	associatedtype Nonzero: Sequence where Nonzero.Element == (Array<Int>, Element)
+//	@inlinable var nonzero: Nonzero { get }
+//}
+//public protocol MutSparseTensor<Element>: MutTensor & SparseTensor {}
+//extension SparseTensor where Element: Numeric {
+//	@inlinable
+//	public func callAsFunction(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
+//		let layout = strategy.stride(for: shape)
+//		let memory = Array<Element>(unsafeUninitializedCapacity: capacity(alloc: shape, stride: layout)) {
+//			$0.initialize(repeating: .zero)
+//			for (key, value) in nonzero {
+//				$0[zip(key, layout).lazy.map(*).reduce(0, +)] = value
+//			}
+//			$1 = $0.count
+//		}
+//		return (layout, {memory})
+//	}
+//}
+//extension SparseTensor where Element == Bool {
+//	@inlinable
+//	public func callAsFunction(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
+//		let layout = strategy.stride(for: shape)
+//		let memory = Array<Element>(unsafeUninitializedCapacity: capacity(alloc: shape, stride: layout)) {
+//			$0.initialize(repeating: false)
+//			for (key, value) in nonzero {
+//				$0[zip(key, layout).lazy.map(*).reduce(0, +)] = value
+//			}
+//			$1 = $0.count
+//		}
+//		return (layout, {memory})
+//	}
+//}
