905aec9259350fb6c3a4c71e3a4836ea785ef8ce
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-21 01:32:34 +0900

    instant vector

--- Dense/Sources/Protocol+Vector.swift
+++ Dense/Sources/Protocol+Vector.swift
@@ -4,54 +4,49 @@
 //
 //  Created by Kota on 9/8/R7.
 //
-public protocol Vector<Element>: Tensor where S: Vector<Element>, T: Vector<Element>, U: Scalar<Element>, V: Vector<Element>{
-	@inlinable var count: Int { get }
-	@inlinable subscript(position: Int) -> U { get }
-	@inlinable subscript(bounds: some RangeExpression<Int>) -> S { get }
-}
-public protocol MutVector<Element>: MutTensor & Vector where S: MutVector<Element>, T: MutVector<Element>, U: MutScalar<Element>, V: MutVector<Element> {
-	@inlinable subscript(position: Int) -> U { get set }
-	@inlinable subscript(bounds: some RangeExpression<Int>) -> S { get set }
-}
 extension Vector {
-	@inlinable
+    @inlinable@inline(__always)
 	public var shape: Array<Int> {
 		[count]
 	}
-	@inlinable
+    @inlinable@inline(__always)
 	public var transpose: Self {
 		self
 	}
-	@inlinable
+    @inlinable@inline(__always)
 	public var diagonal: Self {
 		self
 	}
-	@inlinable
+    @inlinable@inline(__always)
 	public subscript<P>(position: P) -> U where P : RandomAccessCollection, P.Element == Int, P.Index == Int {
-		self[position.first.unsafelyUnwrapped]
+		self[position.last.unsafelyUnwrapped]
 	}
 	@inlinable
 	public subscript<Q>(bounds: Q) -> S where Q : RandomAccessCollection, Q.Element : RangeExpression, Q.Index == Int, Q.Element.Bound == Int {
-		self[bounds.first.unsafelyUnwrapped]
+		self[bounds.last.unsafelyUnwrapped]
 	}
 }
-extension MutVector {
-	@inlinable
+extension InstantVector {
+	
+}
+extension MutableVector {
+    @inlinable@inline(__always)
 	public subscript<P>(position: P) -> U where P : RandomAccessCollection, P.Element == Int, P.Index == Int {
 		_read {
-			yield self[position.first.unsafelyUnwrapped]
+			yield self[position.last.unsafelyUnwrapped]
 		}
 		_modify {
-			yield &self[position.first.unsafelyUnwrapped]
+			yield &self[position.last.unsafelyUnwrapped]
 		}
 	}
-	@inlinable
+    @inlinable@inline(__always)
 	public subscript<Q>(bounds: Q) -> S where Q : RandomAccessCollection, Q.Element : RangeExpression, Q.Index == Int, Q.Element.Bound == Int {
 		_read {
-			yield self[bounds.first.unsafelyUnwrapped]
+			yield self[bounds.last.unsafelyUnwrapped]
 		}
 		_modify {
-			yield &self[bounds.first.unsafelyUnwrapped]
+			yield &self[bounds.last.unsafelyUnwrapped]
 		}
 	}
 }
+//
