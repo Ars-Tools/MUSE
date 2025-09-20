4687b7a3f8578d7b1edf100ad1bf1bc87f3a3bb6
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-21 01:29:41 +0900

    instant matrix

--- Dense/Sources/DOT+M.swift
+++ Dense/Sources/DOT+M.swift
@@ -7,48 +7,54 @@
 import Layout
 extension DOT {
 	@usableFromInline
-	struct DD<LHS: Matrix<Element>, RHS: Matrix<Element>> {
+	struct DD<X: Matrix<Element>, Y: Matrix<Element>> {
 		@usableFromInline typealias R = Array<Element>
-		@usableFromInline typealias S = DD<LHS.S, RHS.S>
-		@usableFromInline typealias U = Inner<LHS.V, RHS.V>
-		@usableFromInline let lhs: LHS
-		@usableFromInline let rhs: RHS
+		@usableFromInline typealias S = DD<X.S, Y.S>
+		@usableFromInline typealias U = Inner<X.V, Y.V>
+		@usableFromInline let x: X
+		@usableFromInline let y: Y
 	}
+    @usableFromInline
+    enum VV<X: Matrix<Element>, Y: Matrix<Element>> {
+        case DD(X, Y)
+        case MV(X, Y)
+        case VM(X, Y)
+    }
 	@usableFromInline
-	struct MM<LHS: Matrix<Element>, RHS: Matrix<Element>> {
+	struct MM<X: Matrix<Element>, Y: Matrix<Element>> {
 		@usableFromInline typealias R = Array<Element>
-		@usableFromInline typealias S = MM<LHS.S, RHS.S>
-		@usableFromInline typealias T = MM<RHS.T, LHS.T>
-		@usableFromInline typealias U = Inner<LHS.V, RHS.V>
-		@usableFromInline typealias V = ANY<Element>.Vector
-		@usableFromInline let lhs: LHS
-		@usableFromInline let rhs: RHS
+		@usableFromInline typealias S = MM<X.S, Y.S>
+		@usableFromInline typealias T = MM<Y.T, X.T>
+		@usableFromInline typealias U = Inner<X.V, Y.V>
+        @usableFromInline typealias V = VV<X.S, Y.S>
+		@usableFromInline let x: X
+		@usableFromInline let y: Y
 	}
 }
 extension DOT.DD: Vector {
 	@inlinable
 	var count: Int {
-		min(lhs.rows, rhs.cols)
+		min(x.rows, y.cols)
 	}
 	@usableFromInline
 	subscript(position: Int) -> U {
-		.init(lhs: lhs[position, 0...], rhs: rhs[position, 0...])
+        .init(x: x[position, 0...], y: y[0..., position])
 	}
 	@usableFromInline
 	subscript(bounds: some RangeExpression<Int>) -> S {
-		.init(lhs: lhs[bounds, 0...], rhs: rhs[0..., bounds])
+		.init(x: x[bounds, 0...], y: y[0..., bounds])
 	}
 	@inlinable
-	func callAsFunction(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
-		let (xi, xk) = try lhs(for: strategy)
-		let (yi, yk) = try lhs(for: strategy)
+	func callAsFunction(as strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
+		let (xi, xk) = try x(as: strategy)
+		let (yi, yk) = try y(as: strategy)
 		precondition((xi.count, yi.count) == (2, 2))
 		let xs = (xi.first ?? 1, xi.last ?? 1)
 		let ys = (yi.first ?? 1, yi.last ?? 1)
 		let zs = [1]
-		let m = min(lhs.rows, rhs.cols)
-		let n = broadcast(x: lhs.cols, y: rhs.rows)
-		return (zs, { 
+		let m = min(x.rows, y.cols)
+		let n = broadcast(x: x.cols, y: y.rows)
+		return (zs, {
 			await withUnsafePointer(xk(), yk()) { x, y in
 				(0..<m).map {
 					Element.Inner(n: n,
@@ -59,46 +65,130 @@ extension DOT.DD: Vector {
 		})
 	}
 }
+extension DOT.DD: InstantTensor & InstantVector where X: InstantMatrix, Y: InstantMatrix {
+    @inlinable
+    func callAsFunction(by strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> Array<Element>) {
+        let (xi, xk) = try x(by: strategy)
+        let (yi, yk) = try y(by: strategy)
+        precondition((xi.count, yi.count) == (2, 2))
+        let xs = (xi.first ?? 1, xi.last ?? 1)
+        let ys = (yi.first ?? 1, yi.last ?? 1)
+        let zs = [1]
+        let m = min(x.rows, y.cols)
+        let n = broadcast(x: x.cols, y: y.rows)
+        return (zs, {
+            withUnsafePointer(xk(), yk()) { x, y in
+                (0..<m).map {
+                    Element.Inner(n: n,
+                                  x: x.advanced(by: $0 * xs.0), ldx: xs.1,
+                                  y: y.advanced(by: $0 * ys.1), ldy: ys.0)
+                }
+            }
+        })
+    }
+}
+extension DOT.VV: Vector {
+    @usableFromInline typealias R = Array<Element>
+    @usableFromInline typealias S = DOT.VV<X.S, Y.S>
+    @usableFromInline typealias U = DOT.Inner<X.V, Y.V>
+    @inlinable
+    var count: Int {
+        switch self {
+        case.DD(let x, let y):
+            min(x.rows, y.cols)
+        case.MV(let x, _):
+            x.rows
+        case.VM(_, let y):
+            y.cols
+        }
+    }
+    @usableFromInline
+    subscript(position: Int) -> U {
+        switch self {
+        case.DD(let x, let y):
+            .init(x: x[position, 0...], y: y[0..., position])
+        case.MV(let x, let y):
+            .init(x: x[position, 0...], y: y[0..., 0])
+        case.VM(let x, let y):
+            .init(x: x[0, 0...], y: y[0..., position])
+        }
+    }
+    @usableFromInline
+    subscript(bounds: some RangeExpression<Int>) -> S {
+        switch self {
+        case.DD(let x, let y):
+            .DD(x[bounds, 0...], y[0..., bounds])
+        case.MV(let x, let y):
+            .MV(x[bounds, 0...], y[0..., 0...])
+        case.VM(let x, let y):
+            .VM(x[0..., 0...], y[0..., bounds])
+        }
+    }
+    @usableFromInline
+    func callAsFunction(as strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
+        switch self {
+        case.DD(let x, let y):
+            try DOT.DD(x: x, y: y)(as: strategy)
+        case.MV(let x, let y):
+            try DOT.MV(x: x, y: y[0..., 0])(as: strategy)
+        case.VM(let x, let y):
+            try DOT.VM(x: x[0, 0...], y: y)(as: strategy)
+        }
+    }
+}
+extension DOT.VV: InstantTensor & InstantVector where X: InstantMatrix, Y: InstantMatrix {
+    @usableFromInline
+    func callAsFunction(by strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> Array<Element>) {
+        switch self {
+        case.DD(let x, let y):
+            try DOT.DD(x: x, y: y)(by: strategy)
+        case.MV(let x, let y):
+            try DOT.MV(x: x, y: y[0..., 0])(by: strategy)
+        case.VM(let x, let y):
+            try DOT.VM(x: x[0, 0...], y: y)(by: strategy)
+        }
+    }
+}
 extension DOT.MM: Matrix {
 	@inlinable
 	var rows: Int {
-		lhs.rows
+		x.rows
 	}
 	@inlinable
 	var cols: Int {
-		rhs.cols
+		y.cols
 	}
 	@usableFromInline
 	var transpose: T {
-		.init(lhs: rhs.transpose, rhs: lhs.transpose)
+		.init(x: y.transpose, y: x.transpose)
 	}
 	@usableFromInline
 	var diagonal: V {
-		.init(core: DOT.DD(lhs: lhs, rhs: rhs))
+        .DD(x[0..., 0...], y[0..., 0...])
 	}
 	@usableFromInline
 	subscript(row: Int, col: Int) -> U {
-		.init(lhs: lhs[row, 0...], rhs: rhs[0..., col])
+		.init(x: x[row, 0...], y: y[0..., col])
 	}
 	@usableFromInline
 	subscript(row: Int, col: some RangeExpression<Int>) -> V {
-		.init(core: lhs[row, 0...] • rhs[0..., col])
+        .VM(x[row...row, 0...], y[0..., col])
 	}
 	@usableFromInline
 	subscript(row: some RangeExpression<Int>, col: Int) -> V {
-		.init(core: lhs[row, 0...] • rhs[0..., col])
+        .MV(x[row, 0...], y[0..., col...col])
 	}
 	@usableFromInline
 	subscript(row: some RangeExpression<Int>, col: some RangeExpression<Int>) -> S {
-		.init(lhs: lhs[row, 0...], rhs: rhs[0..., col])
+		.init(x: x[row, 0...], y: y[0..., col])
 	}
 	@inlinable
-	func callAsFunction(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
-		let (xs, xk) = try lhs(for: strategy)
-		let (ys, yk) = try rhs(for: strategy)
-		let m = lhs.rows
-		let n = rhs.cols
-		let k = broadcast(x: lhs.cols, y: rhs.rows)
+	func callAsFunction(as strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
+		let (xs, xk) = try x(as: strategy)
+		let (ys, yk) = try y(as: strategy)
+		let m = x.rows
+		let n = y.cols
+		let k = broadcast(x: x.cols, y: y.rows)
 		let zs = strategy.stride(for: [m, n])
 		let capacity = capacity(alloc: [m, n], stride: zs)
 		precondition((xs.count, ys.count, zs.count) == (2, 2, 2))
@@ -228,6 +318,146 @@ extension DOT.MM: Matrix {
 		}
 	}
 }
+extension DOT.MM: InstantTensor & InstantMatrix where X: InstantMatrix, Y: InstantMatrix {
+    @inlinable
+    func callAsFunction(by strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> Array<Element>) {
+        let (xs, xk) = try x(by: strategy)
+        let (ys, yk) = try y(by: strategy)
+        let m = x.rows
+        let n = y.cols
+        let k = broadcast(x: x.cols, y: y.rows)
+        let zs = strategy.stride(for: [m, n])
+        let capacity = capacity(alloc: [m, n], stride: zs)
+        precondition((xs.count, ys.count, zs.count) == (2, 2, 2))
+        switch (matricise(size: xs), matricise(size: ys), matricise(size: zs)) {
+        case (.some((1, let lda)), .some((1, let ldb)), .some((1, let ldc))):
+            return (zs, {
+                withUnsafePointer(xk(), yk()) { x, y in
+                    .init(unsafeUninitializedCapacity: capacity) {
+                        let z = $0.baseAddress.unsafelyUnwrapped
+                        Element.GEMM(m: m, n: n, k: k,
+                                     α: 1,
+                                     a: x, lda: lda, opa: "N",
+                                     b: y, ldb: ldb, opb: "N",
+                                     β: 0,
+                                     c: z, ldc: ldc)
+                        $1 = $0.count
+                    }
+                }
+            })
+        case (.some((let lda, 1)), .some((1, let ldb)), .some((1, let ldc))):
+            return (zs, {
+                withUnsafePointer(xk(), yk()) { x, y in
+                    .init(unsafeUninitializedCapacity: capacity) {
+                        let z = $0.baseAddress.unsafelyUnwrapped
+                        Element.GEMM(m: m, n: n, k: k,
+                                     α: 1,
+                                     a: x, lda: lda, opa: "T",
+                                     b: y, ldb: ldb, opb: "N",
+                                     β: 0,
+                                     c: z, ldc: ldc)
+                        $1 = $0.count
+                    }
+                }
+            })
+        case (.some((1, let lda)), .some((let ldb, 1)), .some((1, let ldc))):
+            return (zs, {
+                withUnsafePointer(xk(), yk()) { x, y in
+                    .init(unsafeUninitializedCapacity: capacity) {
+                        let z = $0.baseAddress.unsafelyUnwrapped
+                        Element.GEMM(m: m, n: n, k: k,
+                                     α: 1,
+                                     a: x, lda: lda, opa: "N",
+                                     b: y, ldb: ldb, opb: "T",
+                                     β: 0,
+                                     c: z, ldc: ldc)
+                        $1 = $0.count
+                    }
+                }
+            })
+        case (.some((let lda, 1)), .some((let ldb, 1)), .some((1, let ldc))):
+            return (zs, {
+                withUnsafePointer(xk(), yk()) { x, y in
+                    .init(unsafeUninitializedCapacity: capacity) {
+                        let z = $0.baseAddress.unsafelyUnwrapped
+                        Element.GEMM(m: m, n: n, k: k,
+                                     α: 1,
+                                     a: x, lda: lda, opa: "T",
+                                     b: y, ldb: ldb, opb: "T",
+                                     β: 0,
+                                     c: z, ldc: ldc)
+                        $1 = $0.count
+                    }
+                }
+            })
+        case (.some((1, let lda)), .some((1, let ldb)), .some((let ldc, 1))):
+            return (zs, {
+                withUnsafePointer(xk(), yk()) { x, y in
+                    .init(unsafeUninitializedCapacity: capacity) {
+                        let z = $0.baseAddress.unsafelyUnwrapped
+                        Element.GEMM(m: n, n: m, k: k,
+                                     α: 1,
+                                     a: y, lda: ldb, opa: "T",
+                                     b: x, ldb: lda, opb: "T",
+                                     β: 0,
+                                     c: z, ldc: ldc)
+                        $1 = $0.count
+                    }
+                }
+            })
+        case (.some((let lda, 1)), .some((1, let ldb)), .some((let ldc, 1))):
+            return (zs, {
+                withUnsafePointer(xk(), yk()) { x, y in
+                    .init(unsafeUninitializedCapacity: capacity) {
+                        let z = $0.baseAddress.unsafelyUnwrapped
+                        Element.GEMM(m: n, n: m, k: k,
+                                     α: 1,
+                                     a: y, lda: ldb, opa: "T",
+                                     b: x, ldb: lda, opb: "N",
+                                     β: 0,
+                                     c: z, ldc: ldc)
+                        $1 = $0.count
+                    }
+                }
+            })
+        case (.some((1, let lda)), .some((let ldb, 1)), .some((let ldc, 1))):
+            return (zs, {
+                withUnsafePointer(xk(), yk()) { x, y in
+                    .init(unsafeUninitializedCapacity: capacity) {
+                        let z = $0.baseAddress.unsafelyUnwrapped
+                        Element.GEMM(m: n, n: m, k: k,
+                                     α: 1,
+                                     a: y, lda: ldb, opa: "N",
+                                     b: x, ldb: lda, opb: "T",
+                                     β: 0,
+                                     c: z, ldc: ldc)
+                        $1 = $0.count
+                    }
+                }
+            })
+        case (.some((let lda, 1)), .some((let ldb, 1)), .some((let ldc, 1))):
+            return (zs, {
+                withUnsafePointer(xk(), yk()) { x, y in
+                    .init(unsafeUninitializedCapacity: capacity) {
+                        let z = $0.baseAddress.unsafelyUnwrapped
+                        Element.GEMM(m: n, n: m, k: k,
+                                     α: 1,
+                                     a: y, lda: ldb, opa: "N",
+                                     b: x, ldb: lda, opb: "N",
+                                     β: 0,
+                                     c: z, ldc: ldc)
+                        $1 = $0.count
+                    }
+                }
+            })
+        default:
+            throw Error.invalidShape(self)
+        }
+    }
+}
 public func •<Element: BLASElement & ArithmeticElement>(_ lhs: some Matrix<Element>, _ rhs: some Matrix<Element>) -> some Matrix<Element> {
-	DOT.MM(lhs: lhs, rhs: rhs)
+    DOT.MM(x: lhs, y: rhs)
+}
+public func •<Element: BLASElement & ArithmeticElement>(_ lhs: some InstantMatrix<Element>, _ rhs: some InstantMatrix<Element>) -> some InstantMatrix<Element> {
+    DOT.MM(x: lhs, y: rhs)
 }
