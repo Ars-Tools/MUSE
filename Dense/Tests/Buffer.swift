68567dc0c8e30dab8a777e74a0e98d098c110018
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-09-23 19:31:59 +0900

    implement slicer

--- Dense/Tests/Buffer.swift
+++ Dense/Tests/Buffer.swift
@@ -12,23 +12,25 @@ import Layout
 struct BuuferTestCases {
     @Test
     func tensor() throws {
-        var x = [
+        let x = [
             [[1, 2],
              [3, 4]],
             [[5, 6],
              [7, 8]],
         ] as TensorBuffer<Array<Int>>
-        print(x)
-        print(x[0].transpose)
-        print(x.transpose)
-        print(x.transpose[0])
-//        x[0] = x[1]
-//        print(x)
-//        print(x.diagonal)
-//        print(x)
-//        print(x[0])
-//        print(x[[0]])
-//        print(x)
-//        print(x[[0]])
+        #expect(x[[0, 0, 1]][] == 2)
+        #expect(x[[1, 1, 0]][] == 7)
+    }
+    @Test
+    func slice() throws {
+        let x = MatBuf(rows: [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+        ])
+        let y = Basic.Slice(source: x, order: .rowMajor, slice: [0..<3, 0..<3])[1, 0..<2]
+        let z = try VecBuf(y)
+        #expect(z[0] == x[1, 0])
+        #expect(z[1] == x[1, 1])
     }
 }
