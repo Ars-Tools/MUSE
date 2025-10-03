d03b5c04f58ed685de076b7ebb3577d26628c6aa
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-10-04 06:38:37 +0900

    tensor protocol, split with instant-tensor

--- Dense/Sources/Protocol+Tensor.swift
+++ Dense/Sources/Protocol+Tensor.swift
@@ -2,49 +2,44 @@
 //  Protocol+Tensor.swift
 //  MUSE
 //
-//  Created by Kota on 9/25/25.
+//  Created by Kota on 9/27/25.
 //
-public protocol ElasticTensor<Element>: Tensor {
-    associatedtype S: ElasticTensor<Element>
-    associatedtype T: ElasticTensor<Element>
-    associatedtype U: ElasticTensor<Element>
-    associatedtype V: ElasticTensor<Element>
-    var transpose: T { get }
-    var diagonal: V { get }
-    subscript<P>(position: P) -> U where P: RandomAccessCollection, P.Index: Strideable, P.Index.Stride == Int, P.Element == Int { get }
-    subscript<Q>(bounds: Q) -> S where Q: RandomAccessCollection, Q.Index: Strideable, Q.Index.Stride == Int, Q.Element: RangeExpression<Int> { get }
+@_exported import typealias Layout.MemoryStrategy
+@_exported import func Layout.capacity
+@_exported import func Layout.concat
+@_exported import func Layout.product
+@_exported import func Layout.zip
+public protocol Tensor<Element>: Sendable {
+    associatedtype Element: BitwiseCopyable & Sendable
+    associatedtype Storage: Collection & Sendable where Storage.Index: Strideable, Storage.Index.Stride == Int, Storage.Element == Element
+    associatedtype S: Tensor<Element>
+    associatedtype T: Tensor<Element>
+    associatedtype U: Tensor<Element>
+    associatedtype V: Tensor<Element>
+    @inlinable var shape: Array<Int> { get }
+    @inlinable var transpose: T { get }
+    @inlinable subscript<P>(position: P) -> U where P: RandomAccessCollection, P.Index: Strideable, P.Index.Stride == Int, P.Element == Int { get }
+    @inlinable subscript<Q>(bounds: Q) -> S where Q: RandomAccessCollection, Q.Index: Strideable, Q.Index.Stride == Int, Q.Element: RangeExpression<Int> { get }
+    @inlinable func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Storage)
 }
-public protocol MutableTensor<Element>: ElasticTensor & InstantTensor where S: MutableTensor<Element>, T: MutableTensor<Element>, U: MutableTensor<Element>, V: MutableTensor<Element> {
-    subscript<P>(position: P) -> U where P: RandomAccessCollection, P.Index: Strideable, P.Index.Stride == Int, P.Element == Int { get set }
-    subscript<Q>(bounds: Q) -> S where Q: RandomAccessCollection, Q.Index: Strideable, Q.Index.Stride == Int, Q.Element: RangeExpression<Int> { get set }
+public protocol InstantTensor<Element>: Tensor where S: InstantTensor<Element>, T: InstantTensor<Element>, U: InstantTensor<Element>, V: InstantTensor<Element> {
+    @inlinable func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> Storage)
 }
-extension ElasticTensor {
-    @_disfavoredOverload
-    public subscript(position: Int...) -> U {
-        self[position]
-    }
-    @_disfavoredOverload
-    public subscript<Bounds: RangeExpression<Int>>(bounds: Bounds...) -> S {
-        self[bounds]
+public protocol MutableTensor<Element>: InstantTensor where S: MutableTensor<Element>, T: MutableTensor<Element>, U: MutableTensor<Element>, V: MutableTensor<Element>, Storage: MutableCollection {
+//    @inlinable var diagonal: V { get set }
+    @inlinable subscript<P>(position: P) -> U where P: RandomAccessCollection, P.Index: Strideable, P.Index.Stride == Int, P.Element == Int { get set }
+    @inlinable subscript<Q>(bounds: Q) -> S where Q: RandomAccessCollection, Q.Index: Strideable, Q.Index.Stride == Int, Q.Element: RangeExpression<Int> { get set }
+}
+//
+extension InstantTensor {
+    @inlinable@inline(__always)@_transparent
+    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Storage) {
+        try evaluation(for: strategy) as (Array<Int>, @Sendable () -> Storage)
     }
 }
-extension MutableTensor {
-    @inlinable
-    public subscript(position: Int...) -> U {
-        _read {
-            yield self[position]
-        }
-        _modify {
-            yield &self[position]
-        }
-    }
-    @inlinable
-    public subscript<Bounds: RangeExpression<Int>>(bounds: Bounds...) -> S {
-        _read {
-            yield self[bounds]
-        }
-        _modify {
-            yield &self[bounds]
-        }
-    }
+//
+@usableFromInline
+enum Error: Swift.Error {
+    case shapeMismatch
+    case numericalError(tensor: any Tensor, status: Any, operation: String)
 }
