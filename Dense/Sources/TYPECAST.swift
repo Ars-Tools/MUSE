f0ca85b22dda9fd2a9a7fa32d703c570448d0f61
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-10-04 06:39:50 +0900

    rename

--- Dense/Sources/TYPECAST.swift
+++ Dense/Sources/TYPECAST.swift
@@ -1,181 +1,178 @@
 //
-//  TYPECAST.swift
+//  TypeCast.swift
 //  MUSE
 //
-//  Created by Kota on 9/26/25.
+//  Created by Kota on 9/27/25.
 //
 import Accelerate.vecLib
-import typealias Numerics.Complex64
-import typealias Numerics.Complex128
-@usableFromInline
-enum TYPECAST {}
+public enum TypeCast {}
 public protocol Float32CompatibleElement {
-    static func Convert(_ A: UnsafePointer<Float32>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int)
-    static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float32>, _ IB: Int, _ N: Int)
+    @inlinable@inline(__always)
+    static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float32>, ldy: Int, length: Int)
+    @inlinable@inline(__always)
+    static func Copy(x: UnsafePointer<Float32>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int)
 }
 public protocol Float64CompatibleElement {
-    static func Convert(_ A: UnsafePointer<Float64>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int)
-    static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float64>, _ IB: Int, _ N: Int)
+    @inlinable@inline(__always)
+    static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float64>, ldy: Int, length: Int)
+    @inlinable@inline(__always)
+    static func Copy(x: UnsafePointer<Float64>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int)
 }
 extension Float32CompatibleElement {
-    static func Convert(_ A: UnsafePointer<Complex64>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        A.withMemoryRebound(to: Float32.self, capacity: 2 * IA * N) {
-            Convert($0, 2 * IA, B, IB, N)
+    @inlinable@inline(__always)@_transparent
+    static func Copy(x: UnsafePointer<Complex64>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        x.withMemoryRebound(to: Float32.self, capacity: 2 * ldx * length) {
+            Copy(x: $0, ldx: 2 * ldx, y: y, ldy: ldy, length: length)
         }
     }
-    static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Complex64>, _ IB: Int, _ N: Int) {
-        B.withMemoryRebound(to: Float32.self, capacity: 2 * IB * N) {
-            Convert(A, IA, $0, 2 * IB, N)
-            vDSP_vclr($0.advanced(by: 1), 2 * IB, .init(N))
+    @inlinable@inline(__always)@_transparent
+    static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Complex64>, ldy: Int, length: Int) {
+        y.withMemoryRebound(to: Float32.self, capacity: 2 * ldx * length) {
+            Copy(x: x, ldx: ldx, y: y, ldy: 2 * ldy, length: length)
+            vDSP_vclr($0.advanced(by: 1), 2 * ldy, .init(length))
         }
     }
 }
 extension Float64CompatibleElement {
-    static func Convert(_ A: UnsafePointer<Complex128>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        A.withMemoryRebound(to: Float64.self, capacity: 2 * IA * N) {
-            Convert($0, 2 * IA, B, IB, N)
+    @inlinable@inline(__always)@_transparent
+    static func Copy(x: UnsafePointer<Complex64>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        x.withMemoryRebound(to: Float64.self, capacity: 2 * ldx * length) {
+            Copy(x: $0, ldx: 2 * ldx, y: y, ldy: ldy, length: length)
         }
     }
-    static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Complex128>, _ IB: Int, _ N: Int) {
-        B.withMemoryRebound(to: Float64.self, capacity: 2 * IB * N) {
-            Convert(A, IA, $0, 2 * IB, N)
-            vDSP_vclrD($0.advanced(by: 1), 2 * IB, .init(N))
+    @inlinable@inline(__always)@_transparent
+    static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Complex64>, ldy: Int, length: Int) {
+        y.withMemoryRebound(to: Float64.self, capacity: 2 * ldx * length) {
+            Copy(x: x, ldx: ldx, y: y, ldy: 2 * ldy, length: length)
+            vDSP_vclrD($0.advanced(by: 1), 2 * ldy, .init(length))
         }
     }
 }
 extension Int8: Float32CompatibleElement & Float64CompatibleElement {
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float32>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfix8(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float32>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfix8(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float32>, _ IB: Int, _ N: Int) {
-        vDSP_vflt8(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float32>, ldy: Int, length: Int) {
+        vDSP_vflt8(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float64>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfix8D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float64>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfix8D(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float64>, _ IB: Int, _ N: Int) {
-        vDSP_vflt8D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float64>, ldy: Int, length: Int) {
+        vDSP_vflt8D(x, ldx, y, ldy, .init(length))
     }
 }
 extension Int16: Float32CompatibleElement & Float64CompatibleElement {
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float32>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfix16(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float32>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfix16(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float32>, _ IB: Int, _ N: Int) {
-        vDSP_vflt16(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float32>, ldy: Int, length: Int) {
+        vDSP_vflt16(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float64>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfix16D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float64>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfix16D(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float64>, _ IB: Int, _ N: Int) {
-        vDSP_vflt16D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float64>, ldy: Int, length: Int) {
+        vDSP_vflt16D(x, ldx, y, ldy, .init(length))
     }
 }
 extension Int32: Float32CompatibleElement & Float64CompatibleElement {
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float32>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfix32(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float32>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfix32(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float32>, _ IB: Int, _ N: Int) {
-        vDSP_vflt32(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float32>, ldy: Int, length: Int) {
+        vDSP_vflt32(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float64>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfix32D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float64>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfix32D(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float64>, _ IB: Int, _ N: Int) {
-        vDSP_vflt32D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float64>, ldy: Int, length: Int) {
+        vDSP_vflt32D(x, ldx, y, ldy, .init(length))
     }
 }
 extension UInt8: Float32CompatibleElement & Float64CompatibleElement {
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float32>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfixu8(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float32>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfixu8(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float32>, _ IB: Int, _ N: Int) {
-        vDSP_vfltu8(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float32>, ldy: Int, length: Int) {
+        vDSP_vfltu8(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float64>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfixu8D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float64>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfixu8D(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float64>, _ IB: Int, _ N: Int) {
-        vDSP_vfltu8D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float64>, ldy: Int, length: Int) {
+        vDSP_vfltu8D(x, ldx, y, ldy, .init(length))
     }
 }
 extension UInt16: Float32CompatibleElement & Float64CompatibleElement {
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float32>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfixu16(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float32>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfixu16(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float32>, _ IB: Int, _ N: Int) {
-        vDSP_vfltu16(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float32>, ldy: Int, length: Int) {
+        vDSP_vfltu16(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float64>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfixu16D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float64>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfixu16D(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float64>, _ IB: Int, _ N: Int) {
-        vDSP_vfltu16D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float64>, ldy: Int, length: Int) {
+        vDSP_vfltu16D(x, ldx, y, ldy, .init(length))
     }
 }
 extension UInt32: Float32CompatibleElement & Float64CompatibleElement {
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float32>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfixu32(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float32>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfixu32(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float32>, _ IB: Int, _ N: Int) {
-        vDSP_vflt32(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float32>, ldy: Int, length: Int) {
+        vDSP_vfltu32(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float64>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vfix32D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float64>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vfixu32D(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float64>, _ IB: Int, _ N: Int) {
-        vDSP_vflt32D(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float64>, ldy: Int, length: Int) {
+        vDSP_vfltu32D(x, ldx, y, ldy, .init(length))
     }
 }
 extension Float32: Float32CompatibleElement & Float64CompatibleElement {
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        cblas_scopy(N, A, IA, B, IB)
+    public static func Copy(x: UnsafePointer<Float64>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vdpsp(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float64>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vdpsp(A, IA, B, IB, .init(N))
-    }
-    @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float64>, _ IB: Int, _ N: Int) {
-        vDSP_vspdp(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float64>, ldy: Int, length: Int) {
+        vDSP_vspdp(x, ldx, y, ldy, .init(length))
     }
 }
 extension Float64: Float32CompatibleElement & Float64CompatibleElement {
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Float32>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        vDSP_vspdp(A, IA, B, IB, .init(N))
+    public static func Copy(x: UnsafePointer<Float32>, ldx: Int, y: UnsafeMutablePointer<Self>, ldy: Int, length: Int) {
+        vDSP_vspdp(x, ldx, y, ldy, .init(length))
     }
     @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Float32>, _ IB: Int, _ N: Int) {
-        vDSP_vdpsp(A, IA, B, IB, .init(N))
-    }
-    @inlinable@inline(__always)@_transparent
-    public static func Convert(_ A: UnsafePointer<Self>, _ IA: Int, _ B: UnsafeMutablePointer<Self>, _ IB: Int, _ N: Int) {
-        cblas_dcopy(N, A, IA, B, IB)
+    public static func Copy(x: UnsafePointer<Self>, ldx: Int, y: UnsafeMutablePointer<Float32>, ldy: Int, length: Int) {
+        vDSP_vdpsp(x, ldx, y, ldy, .init(length))
     }
 }
