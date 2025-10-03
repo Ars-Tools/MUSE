45941821e108ce47ac89772c1ea86acdb307c1fa
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-10-04 06:45:52 +0900

    log function

--- Dense/Sources/vFORCE+Log.swift
+++ Dense/Sources/vFORCE+Log.swift
@@ -2,34 +2,49 @@
 //  vFORCE+Log.swift
 //  MUSE
 //
-//  Created by Kota on 9/26/25.
+//  Created by Kota on 9/27/25.
 //
-import typealias Layout.MemoryStrategy
 import func Layout.capacity
 extension vFORCE {
     @frozen public struct Log<X: Tensor<Element>> {
-        public let x: X
+        public typealias S = Log<X.S>
+        public typealias T = Log<X.T>
+        public typealias U = Log<X.U>
+        public typealias V = Log<X.V>
+        @usableFromInline let x: X
         @inlinable@_transparent
         init(x: X) {
             self.x = x
         }
     }
     @frozen public struct Log2<X: Tensor<Element>> {
-        public let x: X
+        public typealias S = Log2<X.S>
+        public typealias T = Log2<X.T>
+        public typealias U = Log2<X.U>
+        public typealias V = Log2<X.V>
+        @usableFromInline let x: X
         @inlinable@_transparent
         init(x: X) {
             self.x = x
         }
     }
     @frozen public struct Log10<X: Tensor<Element>> {
-        public let x: X
+        public typealias S = Log10<X.S>
+        public typealias T = Log10<X.T>
+        public typealias U = Log10<X.U>
+        public typealias V = Log10<X.V>
+        @usableFromInline let x: X
         @inlinable@_transparent
         init(x: X) {
             self.x = x
         }
     }
     @frozen public struct Log1p<X: Tensor<Element>> {
-        public let x: X
+        public typealias S = Log1p<X.S>
+        public typealias T = Log1p<X.T>
+        public typealias U = Log1p<X.U>
+        public typealias V = Log1p<X.V>
+        @usableFromInline let x: X
         @inlinable@_transparent
         init(x: X) {
             self.x = x
@@ -37,73 +52,13 @@ extension vFORCE {
     }
 }
 // MARK: Log
-extension vFORCE.Log: Tensor {
-    @inlinable@inline(__always)@_transparent
-    public var shape: Array<Int> { x.shape }
-    @inlinable@inline(__always)@_transparent
-    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> vFORCE.Storage) {
-        switch try x.evaluation(for: strategy) {
-        case (let xs, let xm):
-            let shape = x.shape
-            let ys = strategy.stride(for: shape)
-            let capacity = capacity(alloc: shape, stride: ys)
-            let (length, stride, offset) = strategy.flatten(shape: shape, xs: xs, ys: ys)
-            return (ys, {
-                await withUnsafePointer(xm()) { x in
-                        .init(unsafeUninitializedCapacity: capacity) {
-                            let y = $0.baseAddress.unsafelyUnwrapped
-                            for offset in offset {
-                                Element.log(x.advanced(by: offset.x), stride.x,
-                                            y.advanced(by: offset.y), stride.y,
-                                            length)
-                            }
-                            $1 = $0.count
-                        }
-                }
-            })
-        }
-    }
-}
-extension vFORCE.Log: InstantTensor where X: InstantTensor {
-    @inlinable@inline(__always)@_transparent
-    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> vFORCE.Storage) {
-        switch try x.evaluation(for: strategy) {
-        case (let xs, let xm):
-            let shape = x.shape
-            let ys = strategy.stride(for: shape)
-            let capacity = capacity(alloc: shape, stride: ys)
-            let (length, stride, offset) = strategy.flatten(shape: shape, xs: xs, ys: ys)
-            return (ys, {
-                withUnsafePointer(xm()) { x in
-                        .init(unsafeUninitializedCapacity: capacity) {
-                            let y = $0.baseAddress.unsafelyUnwrapped
-                            for offset in offset {
-                                Element.log(x.advanced(by: offset.x), stride.x,
-                                            y.advanced(by: offset.y), stride.y,
-                                            length)
-                            }
-                            $1 = $0.count
-                        }
-                }
-            })
-        }
-    }
-}
 extension vFORCE.Log: Scalar & Operator.UnaryScalar where X: Scalar {}
 extension vFORCE.Log: Vector & Operator.UnaryVector where X: Vector {}
 extension vFORCE.Log: Matrix & Operator.UnaryMatrix where X: Matrix {}
-extension vFORCE.Log: ElasticTensor & Operator.UnaryTensor where X: ElasticTensor {
-    public typealias S = vFORCE.Log<X.S>
-    public typealias T = vFORCE.Log<X.T>
-    public typealias U = vFORCE.Log<X.U>
-    public typealias V = vFORCE.Log<X.V>
-}
-// MARK: Log2
-extension vFORCE.Log2: Tensor {
+extension vFORCE.Log: Tensor & Operator.UnaryTensor {
+    public typealias Element = Element
     @inlinable@inline(__always)@_transparent
-    public var shape: Array<Int> { x.shape }
-    @inlinable@inline(__always)@_transparent
-    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> vFORCE.Storage) {
+    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
         switch try x.evaluation(for: strategy) {
         case (let xs, let xm):
             let shape = x.shape
@@ -115,9 +70,9 @@ extension vFORCE.Log2: Tensor {
                         .init(unsafeUninitializedCapacity: capacity) {
                             let y = $0.baseAddress.unsafelyUnwrapped
                             for offset in offset {
-                                Element.log2(x.advanced(by: offset.x), stride.x,
-                                             y.advanced(by: offset.y), stride.y,
-                                             length)
+                                Element.log(x.advanced(by: offset.x), stride.x,
+                                            y.advanced(by: offset.y), stride.y,
+                                            length)
                             }
                             $1 = $0.count
                         }
@@ -126,9 +81,12 @@ extension vFORCE.Log2: Tensor {
         }
     }
 }
-extension vFORCE.Log2: InstantTensor where X: InstantTensor {
+extension vFORCE.Log: InstantScalar where X: InstantScalar {}
+extension vFORCE.Log: InstantVector where X: InstantVector {}
+extension vFORCE.Log: InstantMatrix where X: InstantMatrix {}
+extension vFORCE.Log: InstantTensor where X: InstantTensor {
     @inlinable@inline(__always)@_transparent
-    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> vFORCE.Storage) {
+    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> Array<Element>) {
         switch try x.evaluation(for: strategy) {
         case (let xs, let xm):
             let shape = x.shape
@@ -140,9 +98,9 @@ extension vFORCE.Log2: InstantTensor where X: InstantTensor {
                         .init(unsafeUninitializedCapacity: capacity) {
                             let y = $0.baseAddress.unsafelyUnwrapped
                             for offset in offset {
-                                Element.log2(x.advanced(by: offset.x), stride.x,
-                                             y.advanced(by: offset.y), stride.y,
-                                             length)
+                                Element.log(x.advanced(by: offset.x), stride.x,
+                                            y.advanced(by: offset.y), stride.y,
+                                            length)
                             }
                             $1 = $0.count
                         }
@@ -151,21 +109,14 @@ extension vFORCE.Log2: InstantTensor where X: InstantTensor {
         }
     }
 }
+// MARK: Log2
 extension vFORCE.Log2: Scalar & Operator.UnaryScalar where X: Scalar {}
 extension vFORCE.Log2: Vector & Operator.UnaryVector where X: Vector {}
 extension vFORCE.Log2: Matrix & Operator.UnaryMatrix where X: Matrix {}
-extension vFORCE.Log2: ElasticTensor & Operator.UnaryTensor where X: ElasticTensor {
-    public typealias S = vFORCE.Log2<X.S>
-    public typealias T = vFORCE.Log2<X.T>
-    public typealias U = vFORCE.Log2<X.U>
-    public typealias V = vFORCE.Log2<X.V>
-}
-// MARK: Exp10
-extension vFORCE.Log10: Tensor {
+extension vFORCE.Log2: Tensor & Operator.UnaryTensor {
+    public typealias Element = Element
     @inlinable@inline(__always)@_transparent
-    public var shape: Array<Int> { x.shape }
-    @inlinable@inline(__always)@_transparent
-    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> vFORCE.Storage) {
+    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
         switch try x.evaluation(for: strategy) {
         case (let xs, let xm):
             let shape = x.shape
@@ -177,9 +128,9 @@ extension vFORCE.Log10: Tensor {
                         .init(unsafeUninitializedCapacity: capacity) {
                             let y = $0.baseAddress.unsafelyUnwrapped
                             for offset in offset {
-                                Element.log10(x.advanced(by: offset.x), stride.x,
-                                              y.advanced(by: offset.y), stride.y,
-                                              length)
+                                Element.log2(x.advanced(by: offset.x), stride.x,
+                                             y.advanced(by: offset.y), stride.y,
+                                             length)
                             }
                             $1 = $0.count
                         }
@@ -188,9 +139,12 @@ extension vFORCE.Log10: Tensor {
         }
     }
 }
-extension vFORCE.Log10: InstantTensor where X: InstantTensor {
+extension vFORCE.Log2: InstantScalar where X: InstantScalar {}
+extension vFORCE.Log2: InstantVector where X: InstantVector {}
+extension vFORCE.Log2: InstantMatrix where X: InstantMatrix {}
+extension vFORCE.Log2: InstantTensor where X: InstantTensor {
     @inlinable@inline(__always)@_transparent
-    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> vFORCE.Storage) {
+    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> Array<Element>) {
         switch try x.evaluation(for: strategy) {
         case (let xs, let xm):
             let shape = x.shape
@@ -202,9 +156,9 @@ extension vFORCE.Log10: InstantTensor where X: InstantTensor {
                         .init(unsafeUninitializedCapacity: capacity) {
                             let y = $0.baseAddress.unsafelyUnwrapped
                             for offset in offset {
-                                Element.log10(x.advanced(by: offset.x), stride.x,
-                                              y.advanced(by: offset.y), stride.y,
-                                              length)
+                                Element.log2(x.advanced(by: offset.x), stride.x,
+                                             y.advanced(by: offset.y), stride.y,
+                                             length)
                             }
                             $1 = $0.count
                         }
@@ -213,21 +167,72 @@ extension vFORCE.Log10: InstantTensor where X: InstantTensor {
         }
     }
 }
+// MARK: Log10
 extension vFORCE.Log10: Scalar & Operator.UnaryScalar where X: Scalar {}
 extension vFORCE.Log10: Vector & Operator.UnaryVector where X: Vector {}
 extension vFORCE.Log10: Matrix & Operator.UnaryMatrix where X: Matrix {}
-extension vFORCE.Log10: ElasticTensor & Operator.UnaryTensor where X: ElasticTensor {
-    public typealias S = vFORCE.Log10<X.S>
-    public typealias T = vFORCE.Log10<X.T>
-    public typealias U = vFORCE.Log10<X.U>
-    public typealias V = vFORCE.Log10<X.V>
+extension vFORCE.Log10: Tensor & Operator.UnaryTensor {
+    public typealias Element = Element
+    @inlinable@inline(__always)@_transparent
+    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
+        switch try x.evaluation(for: strategy) {
+        case (let xs, let xm):
+            let shape = x.shape
+            let ys = strategy.stride(for: shape)
+            let capacity = capacity(alloc: shape, stride: ys)
+            let (length, stride, offset) = strategy.flatten(shape: shape, xs: xs, ys: ys)
+            return (ys, {
+                await withUnsafePointer(xm()) { x in
+                        .init(unsafeUninitializedCapacity: capacity) {
+                            let y = $0.baseAddress.unsafelyUnwrapped
+                            for offset in offset {
+                                Element.log10(x.advanced(by: offset.x), stride.x,
+                                              y.advanced(by: offset.y), stride.y,
+                                              length)
+                            }
+                            $1 = $0.count
+                        }
+                }
+            })
+        }
+    }
 }
-// MARK: Log1p
-extension vFORCE.Log1p: Tensor {
+extension vFORCE.Log10: InstantScalar where X: InstantScalar {}
+extension vFORCE.Log10: InstantVector where X: InstantVector {}
+extension vFORCE.Log10: InstantMatrix where X: InstantMatrix {}
+extension vFORCE.Log10: InstantTensor where X: InstantTensor {
     @inlinable@inline(__always)@_transparent
-    public var shape: Array<Int> { x.shape }
+    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> Array<Element>) {
+        switch try x.evaluation(for: strategy) {
+        case (let xs, let xm):
+            let shape = x.shape
+            let ys = strategy.stride(for: shape)
+            let capacity = capacity(alloc: shape, stride: ys)
+            let (length, stride, offset) = strategy.flatten(shape: shape, xs: xs, ys: ys)
+            return (ys, {
+                withUnsafePointer(xm()) { x in
+                        .init(unsafeUninitializedCapacity: capacity) {
+                            let y = $0.baseAddress.unsafelyUnwrapped
+                            for offset in offset {
+                                Element.log10(x.advanced(by: offset.x), stride.x,
+                                              y.advanced(by: offset.y), stride.y,
+                                              length)
+                            }
+                            $1 = $0.count
+                        }
+                }
+            })
+        }
+    }
+}
+// MARK: Expm1
+extension vFORCE.Log1p: Scalar & Operator.UnaryScalar where X: Scalar {}
+extension vFORCE.Log1p: Vector & Operator.UnaryVector where X: Vector {}
+extension vFORCE.Log1p: Matrix & Operator.UnaryMatrix where X: Matrix {}
+extension vFORCE.Log1p: Tensor & Operator.UnaryTensor {
+    public typealias Element = Element
     @inlinable@inline(__always)@_transparent
-    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> vFORCE.Storage) {
+    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<Element>) {
         switch try x.evaluation(for: strategy) {
         case (let xs, let xm):
             let shape = x.shape
@@ -250,9 +255,12 @@ extension vFORCE.Log1p: Tensor {
         }
     }
 }
+extension vFORCE.Log1p: InstantScalar where X: InstantScalar {}
+extension vFORCE.Log1p: InstantVector where X: InstantVector {}
+extension vFORCE.Log1p: InstantMatrix where X: InstantMatrix {}
 extension vFORCE.Log1p: InstantTensor where X: InstantTensor {
     @inlinable@inline(__always)@_transparent
-    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> vFORCE.Storage) {
+    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> Array<Element>) {
         switch try x.evaluation(for: strategy) {
         case (let xs, let xm):
             let shape = x.shape
@@ -275,19 +283,10 @@ extension vFORCE.Log1p: InstantTensor where X: InstantTensor {
         }
     }
 }
-extension vFORCE.Log1p: Scalar & Operator.UnaryScalar where X: Scalar {}
-extension vFORCE.Log1p: Vector & Operator.UnaryVector where X: Vector {}
-extension vFORCE.Log1p: Matrix & Operator.UnaryMatrix where X: Matrix {}
-extension vFORCE.Log1p: ElasticTensor & Operator.UnaryTensor where X: ElasticTensor {
-    public typealias S = vFORCE.Log1p<X.S>
-    public typealias T = vFORCE.Log1p<X.T>
-    public typealias U = vFORCE.Log1p<X.U>
-    public typealias V = vFORCE.Log1p<X.V>
-}
-public func log<X: Tensor>(_ x: X) -> vFORCE<X.Element>.Log<X> {
+public func log<X: Tensor>(_ x: X) -> vFORCE<X.Element>.Log2<X> {
     .init(x: x)
 }
-public func log2<X: Tensor>(_ x: X) -> vFORCE<X.Element>.Log2<X> {
+public func log2<X: Tensor>(_ x: X) -> vFORCE<X.Element>.Exp2<X> {
     .init(x: x)
 }
 public func log10<X: Tensor>(_ x: X) -> vFORCE<X.Element>.Log10<X> {
