3db43fd9f9477d5ebb392efd5f2e6676362f3d73
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-10-04 06:46:58 +0900

    just magnitude

--- Dense/Sources/vFORCE+Magnitude.swift
+++ Dense/Sources/vFORCE+Magnitude.swift
@@ -2,26 +2,30 @@
 //  vFORCE+Magnitude.swift
 //  MUSE
 //
-//  Created by Kota on 9/26/25.
+//  Created by Kota on 9/27/25.
 //
 import typealias Layout.MemoryStrategy
 import func Layout.capacity
 extension vFORCE {
     @frozen public struct Magnitude<X: Tensor<Element>> where X.Element.Magnitude: BitwiseCopyable {
         public typealias Element = X.Element.Magnitude
-        public typealias Storage = Array<Element>
-        public let x: X
+        public typealias S = Magnitude<X.S>
+        public typealias T = Magnitude<X.T>
+        public typealias U = Magnitude<X.U>
+        public typealias V = Magnitude<X.V>
+        @usableFromInline let x: X
         @inlinable
-        public init(x: X) {
+        init(x: X) {
             self.x = x
         }
     }
 }
-extension vFORCE.Magnitude: Tensor {
+extension vFORCE.Magnitude: Scalar & Operator.UnaryScalar where X: Scalar {}
+extension vFORCE.Magnitude: Vector & Operator.UnaryVector where X: Vector {}
+extension vFORCE.Magnitude: Matrix & Operator.UnaryMatrix where X: Matrix {}
+extension vFORCE.Magnitude: Tensor & Operator.UnaryTensor {
     @inlinable@inline(__always)@_transparent
-    public var shape: Array<Int> { x.shape }
-    @inlinable@inline(__always)@_transparent
-    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Storage) {
+    public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () async -> Array<X.Element.Magnitude>) {
         switch try x.evaluation(for: strategy) {
         case (let xs, let xm):
             let xk = x.shape
@@ -43,6 +47,9 @@ extension vFORCE.Magnitude: Tensor {
         }
     }
 }
+extension vFORCE.Magnitude: InstantScalar where X: InstantScalar {}
+extension vFORCE.Magnitude: InstantVector where X: InstantVector {}
+extension vFORCE.Magnitude: InstantMatrix where X: InstantMatrix {}
 extension vFORCE.Magnitude: InstantTensor where X: InstantTensor {
     @inlinable@inline(__always)@_transparent
     public func evaluation(for strategy: MemoryStrategy) throws -> (Array<Int>, @Sendable () -> Array<X.Element.Magnitude>) {
@@ -66,16 +73,6 @@ extension vFORCE.Magnitude: InstantTensor where X: InstantTensor {
             })
         }
     }
-}
-extension vFORCE.Magnitude: Scalar & Operator.UnaryScalar where X: Scalar {}
-extension vFORCE.Magnitude: Vector & Operator.UnaryVector where X: Vector {}
-extension vFORCE.Magnitude: Matrix & Operator.UnaryMatrix where X: Matrix {}
-extension vFORCE.Magnitude: ElasticTensor & Operator.UnaryTensor where X: ElasticTensor {
-    public typealias S = vFORCE.Magnitude<X.S>
-    public typealias T = vFORCE.Magnitude<X.T>
-    public typealias U = vFORCE.Magnitude<X.U>
-    public typealias V = vFORCE.Magnitude<X.V>
-    
 }
 extension Tensor where Element: vFORCEElement & ArithmeticElement, Element.Magnitude: BitwiseCopyable {
     public var magnitude: vFORCE<Element>.Magnitude<Self> {
