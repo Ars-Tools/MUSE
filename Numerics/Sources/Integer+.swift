fc06ea6a7d4127e5ee93bfcb15d5e94133fccdc4
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-10-08 20:16:02 +0900

    improve for performance

--- Numerics/Sources/Integer+.swift
+++ Numerics/Sources/Integer+.swift
@@ -5,17 +5,17 @@
 //  Created by Kota on 5/15/R7.
 //
 @_disfavoredOverload
-@inlinable@inline(__always)
+@inlinable@inline(__always)@_transparent
 public func mod<T: BinaryInteger>(_ x: T, _ y: T) -> T {
 	y == 0 ? 0 : x % y
 }
 @_disfavoredOverload
-@inlinable@inline(__always)
+@inlinable@inline(__always)@_transparent
 public func div<T: BinaryInteger>(_ x: T, _ y: T) -> T {
 	y == 0 ? 0 : x / y
 }
 @_disfavoredOverload
-@inlinable@inline(__always)
+@inlinable@inline(__always)@_transparent
 public func abs<T: BinaryInteger>(_ x: T) -> T { // obtain magnitude without any typecast
 	x < 0 ? ~x + 1 : x
 }
@@ -25,7 +25,7 @@ public func gcd<T: BinaryInteger>(_ x: T, _ y: T) -> T {
 	y == 0 ? x : gcd(y, x % y)
 }
 @_disfavoredOverload
-@inlinable@inline(__always)
+@inlinable@inline(__always)@_transparent
 public func lcm<T: BinaryInteger>(_ x: T, _ y: T) -> T {
 	switch gcd(x, y) {
 	case 0: // i.e. lcm(0, 0)
@@ -35,7 +35,7 @@ public func lcm<T: BinaryInteger>(_ x: T, _ y: T) -> T {
 	}
 }
 extension BinaryInteger {
-	@inlinable@inline(__always)
+    @inlinable@inline(__always)@_transparent
 	public var squareRoot: Self {
 		1 < self ? sequence(state: self / 2) { s in
 			let t = ( s + self / s ) / 2
@@ -45,7 +45,7 @@ extension BinaryInteger {
 			return (s, t)
 		}.first(where: <=).unsafelyUnwrapped.0 : self
 	}
-	@inlinable
+    @inlinable@inline(__always)@_transparent
 	public static var Prime: some Sequence<Self> {
 		sequence(state: Array<Self>()) { state in
 			let prime = sequence(first: (state.last ?? 1) + 1) {
@@ -62,7 +62,7 @@ extension BinaryInteger {
 			return prime
 		}
 	}
-	@inlinable
+    @inlinable@inline(__always)@_transparent
 	public static var Fibonacci: some Sequence<Self> {
 		sequence(first: ((0, 1) as (Self, Self))) {
 			($1, $0 + $1)
