da2d5183be3f5561f3a1cb599b9f0c1cfc667401
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-19 00:39:25 +0900

    templates

---
+++ AMX/Tests/vDSP.swift
@@ -0,0 +1,21 @@
+//
+//  vDSP.swift
+//  MUSE
+//
+//  Created by Kota on 6/18/26.
+//
+import Testing
+import simd
+//import Accelerate
+@testable import AltVec
+@Suite
+struct vDSPTestCases {
+    @Test
+    func fill() {
+        let x = Array<Float32>(unsafeUninitializedCapacity: 32) {
+            AltVec.fill(.pi, $0.baseAddress.unsafelyUnwrapped, 1, $0.count)
+            $1 = $0.count
+        }
+        #expect(x.allSatisfy { $0 == .pi })
+    }
+}
