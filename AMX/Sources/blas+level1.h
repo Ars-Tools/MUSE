66d5845ce4311d29da42b7bae566f396ef339b92
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-18 18:51:40 +0900

    add norms

--- AMX/Sources/blas+level1.h
+++ AMX/Sources/blas+level1.h
@@ -167,3 +167,66 @@ complex128_t const dotc(__LAPACK_int const N,
     zdotc_(&result, &N, x, &incx, y, &incy);
     return result;
 }
+// MARK: 2-norm
+__attribute__((always_inline, __overloadable__)) inline static
+float32_t const nrm2(__LAPACK_int const n,
+                     float32_t const x, __LAPACK_int const incx) {
+    return snrm2(&n, x, &incx);
+}
+__attribute__((always_inline, __overloadable__)) inline static
+float64_t const nrm2(__LAPACK_int const n,
+                     float64_t const x, __LAPACK_int const incx) {
+    return dnrm2(&n, x, &incx);
+}
+__attribute__((always_inline, __overloadable__)) inline static
+float32_t const nrm2(__LAPACK_int const n,
+                     complex64_t const x, __LAPACK_int const incx) {
+    return scnrm2(&n, x, &incx);
+}
+__attribute__((always_inline, __overloadable__)) inline static
+float64_t const nrm2(__LAPACK_int const n,
+                     complex128_t const x, __LAPACK_int const incx) {
+    return dznrm2(&n, x, &incx);
+}
+// MARK: 1-norm
+__attribute__((always_inline, __overloadable__)) inline static
+float32_t const asum(__LAPACK_int const n,
+                     float32_t const x, __LAPACK_int const incx) {
+    return sasum(&n, x, &incx);
+}
+__attribute__((always_inline, __overloadable__)) inline static
+float64_t const asum(__LAPACK_int const n,
+                     float64_t const x, __LAPACK_int const incx) {
+    return dasum(&n, x, &incx);
+}
+__attribute__((always_inline, __overloadable__)) inline static
+float32_t const asum(__LAPACK_int const n,
+                     complex64_t const x, __LAPACK_int const incx) {
+    return scasum(&n, x, &incx);
+}
+__attribute__((always_inline, __overloadable__)) inline static
+float64_t const asum(__LAPACK_int const n,
+                     complex128_t const x, __LAPACK_int const incx) {
+    return dzasum(&n, x, &incx);
+}
+// MARK: ∞-norm
+__attribute__((always_inline, __overloadable__)) inline static
+float32_t const i_amax(__LAPACK_int const N,
+                       float32_t const x, __LAPACK_int const incx) {
+    return si_amax(&N, x, &incx);
+}
+__attribute__((always_inline, __overloadable__)) inline static
+float64_t const i_amax(__LAPACK_int const N,
+                       float64_t const x, __LAPACK_int const incx) {
+    return di_amax(&N, x, &incx);
+}
+__attribute__((always_inline, __overloadable__)) inline static
+float32_t const i_amax(__LAPACK_int const N,
+                       complex64_t const x, __LAPACK_int const incx) {
+    return sci_amax(&N, x, &incx);
+}
+__attribute__((always_inline, __overloadable__)) inline static
+float64_t const i_amax(__LAPACK_int const N,
+                       complex128_t const x, __LAPACK_int const incx) {
+    return dzi_amax(&N, x, &incx);
+}
