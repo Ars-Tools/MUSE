d412fec946c6f55a0dd81a3abd702b8c29f56809
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-19 00:37:46 +0900

    altivec

---
+++ AMX/Sources/vDSP+gen.h
@@ -0,0 +1,23 @@
+//
+//  vDSP.h
+//  MUSE
+//
+//  Created by Kota on 6/18/26.
+//
+#include"module.h"
+__attribute__((always_inline, __overloadable__)) static inline // for each (0<=k<length), A[k*iA] = 0
+void clr(float32_t * __nonnull const A, intptr_t const iA, intptr_t const length) {
+    vDSP_vclr(A, iA, length);
+}
+__attribute__((always_inline, __overloadable__)) static inline // for each (0<=k<length), A[k*iA] = 0
+void clr(float64_t * __nonnull const A, intptr_t const iA, intptr_t const length) {
+    vDSP_vclrD(A, iA, length);
+}
+__attribute__((always_inline, __overloadable__)) static inline // for each (0<=k<length), B[k*iB] = A
+void fill(float32_t const A, float32_t * __nonnull const B, intptr_t const iB, intptr_t const length) {
+    vDSP_vfill(&A, B, iB, length);
+}
+__attribute__((always_inline, __overloadable__)) static inline // for each (0<=k<length), B[k*iB] = A
+void fill(float64_t const A, float64_t * __nonnull const B, intptr_t const iB, intptr_t const length) {
+    vDSP_vfillD(&A, B, iB, length);
+}
