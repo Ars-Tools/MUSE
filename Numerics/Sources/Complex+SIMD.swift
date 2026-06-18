c95834d9e4f64214a6591052ed515377bba931a9
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-19 00:41:06 +0900

    optimisation

--- Numerics/Sources/Complex+SIMD.swift
+++ Numerics/Sources/Complex+SIMD.swift
@@ -11,6 +11,7 @@ import func simd.__sincos_stret
 import func simd.__sincosf_stret
 import func simd.atan2f
 import func simd.atan2l
+import AltVec
 extension ComplexNumber where FloatLiteralType: SIMDScalar & BinaryFloatingPoint {
     @inlinable@inline(__always)@_transparent
 	public static func+(lhs: Self, rhs: Self) -> Self {
@@ -28,18 +29,32 @@ extension ComplexNumber where FloatLiteralType == Float32 {
 		self.init(real: e.y, imag: e.x)
 	}
     @inlinable@inline(__always)@_transparent
+    public static func+(lhs: Self, rhs: Self) -> Self {
+        unsafeBitCast(complex_add(
+            unsafeBitCast(lhs, to: complex64_t.self),
+            unsafeBitCast(rhs, to: complex64_t.self)
+        ), to: Self.self)
+    }
+    @inlinable@inline(__always)@_transparent
+    public static func-(lhs: Self, rhs: Self) -> Self {
+        unsafeBitCast(complex_sub(
+            unsafeBitCast(lhs, to: complex64_t.self),
+            unsafeBitCast(rhs, to: complex64_t.self)
+        ), to: Self.self)
+    }
+    @inlinable@inline(__always)@_transparent
 	public static func*(lhs: Self, rhs: Self) -> Self {
-		unsafeBitCast(SIMD2(
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(rhs.real, -rhs.imag)),
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(rhs.imag,  rhs.real))
-		), to: Self.self)
+		unsafeBitCast(complex_mul(
+            unsafeBitCast(lhs, to: complex64_t.self),
+            unsafeBitCast(rhs, to: complex64_t.self)
+        ), to: Self.self)
 	}
     @inlinable@inline(__always)@_transparent
 	public static func/(lhs: Self, rhs: Self) -> Self {
-		unsafeBitCast(SIMD2(
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2( rhs.real, rhs.imag)),
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(-rhs.imag, rhs.real))
-		) / length_squared(unsafeBitCast(rhs, to: SIMD2<FloatLiteralType>.self)), to: Self.self)
+        unsafeBitCast(complex_div(
+            unsafeBitCast(lhs, to: complex64_t.self),
+            unsafeBitCast(rhs, to: complex64_t.self)
+        ), to: Self.self)
 	}
 }
 extension ComplexNumber where FloatLiteralType == Float64 {
@@ -49,17 +64,31 @@ extension ComplexNumber where FloatLiteralType == Float64 {
 		self.init(real: e.y, imag: e.x)
 	}
     @inlinable@inline(__always)@_transparent
+    public static func+(lhs: Self, rhs: Self) -> Self {
+        unsafeBitCast(complex_add(
+            unsafeBitCast(lhs, to: complex128_t.self),
+            unsafeBitCast(rhs, to: complex128_t.self)
+        ), to: Self.self)
+    }
+    @inlinable@inline(__always)@_transparent
+    public static func-(lhs: Self, rhs: Self) -> Self {
+        unsafeBitCast(complex_sub(
+            unsafeBitCast(lhs, to: complex128_t.self),
+            unsafeBitCast(rhs, to: complex128_t.self)
+        ), to: Self.self)
+    }
+    @inlinable@inline(__always)@_transparent
 	public static func*(lhs: Self, rhs: Self) -> Self {
-		unsafeBitCast(SIMD2(
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(rhs.real, -rhs.imag)),
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(rhs.imag,  rhs.real))
-		), to: Self.self)
+        unsafeBitCast(complex_mul(
+            unsafeBitCast(lhs, to: complex128_t.self),
+            unsafeBitCast(rhs, to: complex128_t.self)
+        ), to: Self.self)
 	}
     @inlinable@inline(__always)@_transparent
 	public static func/(lhs: Self, rhs: Self) -> Self {
-		unsafeBitCast(SIMD2(
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2( rhs.real, rhs.imag)),
-			dot(unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self), SIMD2(-rhs.imag, rhs.real))
-		) / length_squared(unsafeBitCast(rhs, to: SIMD2<FloatLiteralType>.self)), to: Self.self)
+        unsafeBitCast(complex_div(
+            unsafeBitCast(lhs, to: complex128_t.self),
+            unsafeBitCast(rhs, to: complex128_t.self)
+        ), to: Self.self)
 	}
 }
