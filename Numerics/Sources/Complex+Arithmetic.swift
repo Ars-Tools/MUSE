dcf450721a806a2352c8d4439c01ce62d95c2ca8
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-22 20:03:45 +0900

    Arithmetics with @disfavoredOverloaded

--- Numerics/Sources/Complex+Arithmetic.swift
+++ Numerics/Sources/Complex+Arithmetic.swift
@@ -5,18 +5,21 @@
 //  Created by Kota on 5/15/R7.
 //
 extension ComplexNumber {
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func+(lhs: Self, rhs: Self) -> Self {
 		let real = lhs.real + rhs.real
 		let imag = lhs.imag + rhs.imag
 		return.init(real: real, imag: imag)
 	}
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func-(lhs: Self, rhs: Self) -> Self {
 		let real = lhs.real - rhs.real
 		let imag = lhs.imag - rhs.imag
 		return.init(real: real, imag: imag)
 	}
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func*(lhs: Self, rhs: Self) -> Self {
 		let real = lhs.real * rhs.real - lhs.imag * rhs.imag
@@ -25,20 +28,24 @@ extension ComplexNumber {
 	}
 }
 extension ComplexNumber {
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func+=(lhs: inout Self, rhs: Self) {
 		lhs = lhs + rhs
 	}
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func-=(lhs: inout Self, rhs: Self) {
 		lhs = lhs - rhs
 	}
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func*=(lhs: inout Self, rhs: Self) {
 		lhs = lhs * rhs
 	}
 }
 extension ComplexNumber where FloatLiteralType: BinaryInteger {
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func/(lhs: Self, rhs: Self) -> Self {
 		let norm = rhs.real * rhs.real + rhs.imag * rhs.imag
@@ -46,12 +53,14 @@ extension ComplexNumber where FloatLiteralType: BinaryInteger {
 		let imag = lhs.real * rhs.imag - lhs.imag * rhs.real
 		return.init(real: real / norm, imag: imag / norm)
 	}
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func/=(lhs: inout Self, rhs: Self) {
 		lhs = lhs / rhs
 	}
 }
 extension ComplexNumber where FloatLiteralType: BinaryFloatingPoint {
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func/(lhs: Self, rhs: Self) -> Self {
 		let norm = rhs.real * rhs.real + rhs.imag * rhs.imag
@@ -59,18 +68,21 @@ extension ComplexNumber where FloatLiteralType: BinaryFloatingPoint {
 		let imag = lhs.real * rhs.imag - lhs.imag * rhs.real
 		return.init(real: real / norm, imag: imag / norm)
 	}
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func/=(lhs: inout Self, rhs: Self) {
 		lhs = lhs / rhs
 	}
 }
 extension ComplexNumber {
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static prefix func-(_ χ: Self) -> Self {
 		.init(real: -χ.real, imag: -χ.imag)
 	}
 }
 extension ComplexNumber {
+    @_disfavoredOverload
     @inlinable@inline(__always)@_transparent
 	public static func==(lhs: Self, rhs: Self) -> Bool {
 		(lhs.real, lhs.imag) == (rhs.real, rhs.imag)
