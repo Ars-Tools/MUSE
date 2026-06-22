4f0dcf6eca394d8abd251b8d0dae8c51a4522521
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-22 20:02:52 +0900

    vDSP test cases

--- AMX/Tests/vDSP.swift
+++ AMX/Tests/vDSP.swift
@@ -13,7 +13,7 @@ struct vDSPTestCases {
     @Test
     func fill() {
         let x = Array<Float32>(unsafeUninitializedCapacity: 32) {
-            AltVec.fill(.pi, $0.baseAddress.unsafelyUnwrapped, 1, $0.count)
+            AltVec.vDSP_fill(.pi, $0.baseAddress.unsafelyUnwrapped, 1, $0.count)
             $1 = $0.count
         }
         #expect(x.allSatisfy { $0 == .pi })
