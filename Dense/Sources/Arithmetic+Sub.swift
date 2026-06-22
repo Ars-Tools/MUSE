74c821012c28f18aff5f3db35e0a914c0760a4ed
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-22 20:07:00 +0900

    sub

--- Dense/Sources/Arithmetic+Sub.swift
+++ Dense/Sources/Arithmetic+Sub.swift
@@ -45,9 +45,9 @@ extension Arithmetic.Sub: Operator.BinaryTensor {
                 .init(unsafeUninitializedCapacity: zm) {
                     let z = $0.baseAddress.unsafelyUnwrapped
                     for offset in offset {
-                        Element.Sub(x: x.advanced(by: offset.x), ldx: stride.x,
-                                    y: y.advanced(by: offset.y), ldy: stride.y,
-                                    z: z.advanced(by: offset.z), ldz: stride.z, length: length)
+                        Element.Sub(x: x.advanced(by: offset.x), inc: stride.x,
+                                    y: y.advanced(by: offset.y), inc: stride.y,
+                                    z: z.advanced(by: offset.z), inc: stride.z, length: length)
                     }
                     $1 = $0.count
                 }
@@ -77,9 +77,9 @@ extension Arithmetic.Sub: InstantTensor where X: InstantTensor, Y: InstantTensor
                 .init(unsafeUninitializedCapacity: zm) {
                     let z = $0.baseAddress.unsafelyUnwrapped
                     for offset in offset {
-                        Element.Sub(x: x.advanced(by: offset.x), ldx: stride.x,
-                                    y: y.advanced(by: offset.y), ldy: stride.y,
-                                    z: z.advanced(by: offset.z), ldz: stride.z, length: length)
+                        Element.Sub(x: x.advanced(by: offset.x), inc: stride.x,
+                                    y: y.advanced(by: offset.y), inc: stride.y,
+                                    z: z.advanced(by: offset.z), inc: stride.z, length: length)
                     }
                     $1 = $0.count
                 }
