9fbbf6cbbf5e8f9125d46d16bf4cbc2ddced99e7
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-22 19:59:43 +0900

    lu decomposition

--- AMX/Sources/lapack+lu.h
+++ AMX/Sources/lapack+lu.h
@@ -112,53 +112,133 @@ __attribute__((always_inline, __overloadable__, warn_unused_result)) inline stat
 __LAPACK_int const getri(__LAPACK_int const n,
                          float32_t const*__nonnull const A, __LAPACK_int const ldA,
                          __LAPACK_int const*__nonnull const ipiv,
-                         float32_t      *__nonnull const work, __LAPACK_int const lwork) {
-    __LAPACK_int info;
-    sgetri_(&n,
-            A, &ldA,
-            ipiv,
-            work, &lwork,
-            &info);
-    return info;
+                         float32_t      *__nullable const work, __LAPACK_int const lwork) {
+    if ( lwork < 0 ) { // query
+        __LAPACK_int info;
+        sgetri_(&n,
+                A, &ldA,
+                ipiv,
+                work, &lwork,
+                &info);
+        return info;
+    } else if ( lwork < 0 ) { // allocated
+        __LAPACK_int info;
+        float32_t size = MAX(1, n);
+        sgetri_(&n,
+                A, &ldA,
+                ipiv,
+                &size, &lwork,
+                &info);
+        return info ? info : size;
+    } else { // auto alloca
+        __LAPACK_int info;
+        __LAPACK_int const size = MAX(1, n);
+        sgetri_(&n,
+                A, &ldA,
+                ipiv,
+                alloca(size * sizeof(float32_t const)), &size,
+                &info);
+        return info;
+    }
 }
 __attribute__((always_inline, __overloadable__, warn_unused_result)) inline static
 __LAPACK_int const getri(__LAPACK_int const n,
                          float64_t const*__nonnull const A, __LAPACK_int const ldA,
                          __LAPACK_int const*__nonnull const ipiv,
-                         float64_t      *__nonnull const work, __LAPACK_int const lwork) {
-    __LAPACK_int info;
-    dgetri_(&n,
-            A, &ldA,
-            ipiv,
-            work, &lwork,
-            &info);
-    return info;
+                         float64_t      *__nullable const work, __LAPACK_int const lwork) {
+    if ( lwork < 0 ) { // query
+        __LAPACK_int info;
+        dgetri_(&n,
+                A, &ldA,
+                ipiv,
+                work, &lwork,
+                &info);
+        return info;
+    } else if ( 0 < lwork ) { // allocated
+        __LAPACK_int info;
+        float32_t size = MAX(1, n);
+        dgetri_(&n,
+                A, &ldA,
+                ipiv,
+                &size, &lwork,
+                &info);
+        return info ? info : size;
+    } else { // auto alloca
+        __LAPACK_int info;
+        __LAPACK_int const size = MAX(1, n);
+        dgetri_(&n,
+                A, &ldA,
+                ipiv,
+                alloca(size * sizeof(float64_t const)), &size,
+                &info);
+        return info;
+    }
 }
 __attribute__((always_inline, __overloadable__, warn_unused_result)) inline static
 __LAPACK_int const getri(__LAPACK_int const n,
                          complex64_t const*__nonnull const A, __LAPACK_int const ldA,
                          __LAPACK_int const*__nonnull const ipiv,
-                         complex64_t      *__nonnull const work, __LAPACK_int const lwork) {
-    __LAPACK_int info;
-    cgetri_(&n,
-            A, &ldA,
-            ipiv,
-            work, &lwork,
-            &info);
-    return info;
+                         complex64_t      *__nullable const work, __LAPACK_int const lwork) {
+    if ( lwork < 0 ) { // query
+        __LAPACK_int info;
+        cgetri_(&n,
+                A, &ldA,
+                ipiv,
+                work, &lwork,
+                &info);
+        return info;
+    } else if ( 0 < lwork ) { // allocated
+        __LAPACK_int info;
+        __complex float size = MAX(1, n);
+        cgetri_(&n,
+                A, &ldA,
+                ipiv,
+                &size, &lwork,
+                &info);
+        return info ? info : size;
+    } else { // auto alloca
+        __LAPACK_int info;
+        __LAPACK_int const size = MAX(1, n);
+        cgetri_(&n,
+                A, &ldA,
+                ipiv,
+                alloca(size * sizeof(__complex float const)), &size,
+                &info);
+        return info;
+    }
 }
 __attribute__((always_inline, __overloadable__, warn_unused_result)) inline static
 __LAPACK_int const getri(__LAPACK_int const n,
                          complex128_t const*__nonnull const A, __LAPACK_int const ldA,
                          __LAPACK_int const*__nonnull const ipiv,
-                         complex128_t      *__nonnull const work, __LAPACK_int const lwork) {
-    __LAPACK_int info;
-    zgetri_(&n,
-            A, &ldA,
-            ipiv,
-            work, &lwork,
-            &info);
-    return info;
+                         complex128_t      *__nullable const work, __LAPACK_int const lwork) {
+    if ( lwork < 0 ) { // query
+        __LAPACK_int info;
+        zgetri_(&n,
+                A, &ldA,
+                ipiv,
+                work, &lwork,
+                &info);
+        return info;
+    } else if ( 0 < lwork ) { // allocated
+        __LAPACK_int info;
+        __complex double size = MAX(1, n);
+        zgetri_(&n,
+                A, &ldA,
+                ipiv,
+                &size, &lwork,
+                &info);
+        return info ? info : size;
+    } else { // auto alloca
+        __LAPACK_int info;
+        __LAPACK_int const size = MAX(1, n);
+        zgetri_(&n,
+                A, &ldA,
+                ipiv,
+                alloca(size * sizeof(__complex double const)), &size,
+                &info);
+        return info;
+    }
 }
 // MARK: laswp
 __attribute__((always_inline, __overloadable__, warn_unused_result)) inline static
