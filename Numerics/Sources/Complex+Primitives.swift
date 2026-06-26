dbff56980f21d117b3e39317f0a63d10805410c0
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-26 18:13:14 +0900

    rawValue

--- Numerics/Sources/Complex+Primitives.swift
+++ Numerics/Sources/Complex+Primitives.swift
@@ -29,7 +29,7 @@ import AltVec
         simd_length(unsafeBitCast(self, to: simd_half2.self))
 	}
     @inline(__always)
-	public static let i = Self(real: 0, imag: 1)
+	public static let I = Self(real: 0, imag: 1)
 }
 extension Complex32 {
     @inlinable@inline(__always)@_transparent
@@ -67,39 +67,44 @@ extension Complex32: AtomicRepresentable {
         unsafeBitCast(storage, to: Self.self)
     }
 }
-@frozen public struct Complex64: ComplexNumber & BitwiseCopyable {
-	public typealias FloatLiteralType = Float32
-	public let real: FloatLiteralType
-	public let imag: FloatLiteralType
-	@inlinable@inline(__always)@_transparent
-	public init(real r: FloatLiteralType, imag i: FloatLiteralType) {
-		real = r
-		imag = i
-	}
+@frozen public struct Complex64: ComplexNumber & BitwiseCopyable & RawRepresentable & Hashable {
+    public typealias FloatLiteralType = Float32
+    public let rawValue: complex64_t
     @inlinable@inline(__always)@_transparent
-	public var magnitude: FloatLiteralType.Magnitude {
-		length(unsafeBitCast(self, to: SIMD2<FloatLiteralType>.self))
-	}
-    @inlinable@inline(__always)@_transparent
-    public var magnitudeSquared: FloatLiteralType.Magnitude {
-        length_squared(unsafeBitCast(self, to: SIMD2<FloatLiteralType>.self))
+    public init(rawValue value: RawValue) {
+        rawValue = value
     }
-	public static let i = Self(real: 0, imag: 1)
+    @inlinable@inline(__always)@_transparent
+    public init(real: FloatLiteralType, imag: FloatLiteralType) {
+        rawValue = .init(.init(r: real, i: imag))
+    }
+    @inlinable@inline(__always)@_transparent
+    public var real: FloatLiteralType {
+        rawValue.r
+    }
+    @inlinable@inline(__always)@_transparent
+    public var imag: FloatLiteralType {
+        rawValue.i
+    }
+    @inlinable@inline(__always)@_transparent
+    public var magnitude: FloatLiteralType {
+        length(rawValue.vector)
+    }
+    @inlinable@inline(__always)@_transparent
+    public var magnitude_squared: FloatLiteralType {
+        length_squared(rawValue.vector)
+    }
+    @inlinable@inline(__always)@_transparent
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(rawValue.vector)
+    }
+    @inline(__always)
+    public static let I = Self(real: 0, imag: 1)
 }
-extension Complex64: RawRepresentable {
-    public typealias RawValue = complex64_t
-    @inlinable@inline(__always)@_transparent
-    public init(rawValue: RawValue) {
-        self = unsafeBitCast(rawValue, to: Self.self)
-    }
-    @inlinable@inline(__always)@_transparent
-    public var rawValue: RawValue {
-        unsafeBitCast(self, to: RawValue.self)
-    }
+extension Complex64 {
     @inlinable@inline(__always)@_transparent
     public static func==(lhs: Self, rhs: Self) -> Bool {
-        unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self) ==
-        unsafeBitCast(rhs, to: SIMD2<FloatLiteralType>.self)
+        lhs.rawValue.vector == rhs.rawValue.vector
     }
     @inlinable@inline(__always)@_transparent
     public static prefix func-(lhs: Self) -> Self {
@@ -123,11 +128,11 @@ extension Complex64: RawRepresentable {
     }
     @inlinable@inline(__always)@_transparent
     public init(r: FloatLiteralType, θ: FloatLiteralType) {
-        self.init(rawValue: complex_new_rt(r, θ))
+        rawValue = complex_new_rt(r, θ)
     }
     @inlinable@inline(__always)@_transparent
     public init(r: FloatLiteralType, πr: FloatLiteralType) {
-        self.init(rawValue: complex_new_rp(r, πr))
+        rawValue = complex_new_rp(r, πr)
     }
     @inlinable@inline(__always)@_transparent
     public var θ: FloatLiteralType {
@@ -135,42 +140,103 @@ extension Complex64: RawRepresentable {
     }
     @inlinable@inline(__always)@_transparent
     public var conj: Self {
-        unsafeBitCast(complex_conj(rawValue), to: Self.self)
+        .init(rawValue: complex_conj(rawValue))
     }
     @inlinable@inline(__always)@_transparent
     public var proj: Self {
-        unsafeBitCast(complex_proj(rawValue), to: Self.self)
+        .init(rawValue: complex_proj(rawValue))
     }
 }
 extension Complex64: AtomicRepresentable {
 	public typealias AtomicRepresentation = FloatLiteralType.SIMD2Storage
 	@inlinable@inline(__always)@_transparent
 	public static func encodeAtomicRepresentation(_ value: consuming Self) -> AtomicRepresentation {
-		unsafeBitCast(value, to: AtomicRepresentation.self)
+        unsafeBitCast(value.rawValue.vector, to: AtomicRepresentation.self)
 	}
 	@inlinable@inline(__always)@_transparent
 	public static func decodeAtomicRepresentation(_ storage: consuming AtomicRepresentation) -> Self {
-		unsafeBitCast(storage, to: Self.self)
+        unsafeBitCast(storage, to: Self.self)
 	}
 }
-@frozen public struct Complex128: ComplexNumber & BitwiseCopyable {
-	public typealias FloatLiteralType = Float64
-	public let real: FloatLiteralType
-	public let imag: FloatLiteralType
-	@inlinable@inline(__always)@_transparent
-	public init(real r: FloatLiteralType, imag i: FloatLiteralType) {
-		real = r
-		imag = i
-	}
-	@inlinable@inline(__always)@_transparent
-	public var magnitude: FloatLiteralType.Magnitude {
-		length(unsafeBitCast(self, to: SIMD2<FloatLiteralType>.self))
-	}
+@frozen public struct Complex128: ComplexNumber & BitwiseCopyable & RawRepresentable & Hashable {
+    public typealias FloatLiteralType = Float64
+    public let rawValue: complex128_t
     @inlinable@inline(__always)@_transparent
-    public var magnitudeSquared: FloatLiteralType.Magnitude {
-        length_squared(unsafeBitCast(self, to: SIMD2<FloatLiteralType>.self))
+    public init(rawValue value: RawValue) {
+        rawValue = value
     }
-	public static let i = Self(real: 0, imag: 1)
+    @inlinable@inline(__always)@_transparent
+    public init(real: FloatLiteralType, imag: FloatLiteralType) {
+        rawValue = .init(.init(r: real, i: imag))
+    }
+    @inlinable@inline(__always)@_transparent
+    public var real: FloatLiteralType {
+        rawValue.r
+    }
+    @inlinable@inline(__always)@_transparent
+    public var imag: FloatLiteralType {
+        rawValue.i
+    }
+    @inlinable@inline(__always)@_transparent
+    public var magnitude: FloatLiteralType {
+        length(rawValue.vector)
+    }
+    @inlinable@inline(__always)@_transparent
+    public var magnitude_squared: FloatLiteralType {
+        length_squared(rawValue.vector)
+    }
+    @inlinable@inline(__always)@_transparent
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(rawValue.vector)
+    }
+    @inline(__always)
+    public static let I = Self(real: 0, imag: 1)
+}
+extension Complex128 {
+    @inlinable@inline(__always)@_transparent
+    public static func==(lhs: Self, rhs: Self) -> Bool {
+        lhs.rawValue.vector == rhs.rawValue.vector
+    }
+    @inlinable@inline(__always)@_transparent
+    public static prefix func-(lhs: Self) -> Self {
+        .init(rawValue: complex_neg(lhs.rawValue))
+    }
+    @inlinable@inline(__always)@_transparent
+    public static func+(lhs: Self, rhs: Self) -> Self {
+        .init(rawValue: complex_add(lhs.rawValue, rhs.rawValue))
+    }
+    @inlinable@inline(__always)@_transparent
+    public static func-(lhs: Self, rhs: Self) -> Self {
+        .init(rawValue: complex_sub(lhs.rawValue, rhs.rawValue))
+    }
+    @inlinable@inline(__always)@_transparent
+    public static func*(lhs: Self, rhs: Self) -> Self {
+        .init(rawValue: complex_mul(lhs.rawValue, rhs.rawValue))
+    }
+    @inlinable@inline(__always)@_transparent
+    public static func/(lhs: Self, rhs: Self) -> Self {
+        .init(rawValue: complex_div(lhs.rawValue, rhs.rawValue))
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(r: FloatLiteralType, θ: FloatLiteralType) {
+        rawValue = complex_new_rt(r, θ)
+    }
+    @inlinable@inline(__always)@_transparent
+    public init(r: FloatLiteralType, πr: FloatLiteralType) {
+        rawValue = complex_new_rp(r, πr)
+    }
+    @inlinable@inline(__always)@_transparent
+    public var θ: FloatLiteralType {
+        complex_arg(rawValue);
+    }
+    @inlinable@inline(__always)@_transparent
+    public var conj: Self {
+        .init(rawValue: complex_conj(rawValue))
+    }
+    @inlinable@inline(__always)@_transparent
+    public var proj: Self {
+        .init(rawValue: complex_proj(rawValue))
+    }
 }
 extension Complex128: AtomicRepresentable {
 	public typealias AtomicRepresentation = FloatLiteralType.SIMD2Storage
@@ -182,62 +248,6 @@ extension Complex128: AtomicRepresentable {
 	public static func decodeAtomicRepresentation(_ storage: consuming AtomicRepresentation) -> Self {
 		unsafeBitCast(storage, to: Self.self)
 	}
-}
-extension Complex128: RawRepresentable {
-    public typealias RawValue = complex128_t
-    @inlinable@inline(__always)@_transparent
-    public init(rawValue: RawValue) {
-        self = unsafeBitCast(rawValue, to: Self.self)
-    }
-    @inlinable@inline(__always)@_transparent
-    public var rawValue: RawValue {
-        unsafeBitCast(self, to: RawValue.self)
-    }
-    @inlinable@inline(__always)@_transparent
-    public static func==(lhs: Self, rhs: Self) -> Bool {
-        unsafeBitCast(lhs, to: SIMD2<FloatLiteralType>.self) ==
-        unsafeBitCast(rhs, to: SIMD2<FloatLiteralType>.self)
-    }
-    @inlinable@inline(__always)@_transparent
-    public static prefix func-(lhs: Self) -> Self {
-        .init(rawValue: complex_neg(lhs.rawValue))
-    }
-    @inlinable@inline(__always)@_transparent
-    public static func+(lhs: Self, rhs: Self) -> Self {
-        .init(rawValue: complex_add(lhs.rawValue, rhs.rawValue))
-    }
-    @inlinable@inline(__always)@_transparent
-    public static func-(lhs: Self, rhs: Self) -> Self {
-        .init(rawValue: complex_sub(lhs.rawValue, rhs.rawValue))
-    }
-    @inlinable@inline(__always)@_transparent
-    public static func*(lhs: Self, rhs: Self) -> Self {
-        .init(rawValue: complex_mul(lhs.rawValue, rhs.rawValue))
-    }
-    @inlinable@inline(__always)@_transparent
-    public static func/(lhs: Self, rhs: Self) -> Self {
-        .init(rawValue: complex_div(lhs.rawValue, rhs.rawValue))
-    }
-    @inlinable@inline(__always)@_transparent
-    public init(r: FloatLiteralType, θ: FloatLiteralType) {
-        self.init(rawValue: complex_new_rt(r, θ))
-    }
-    @inlinable@inline(__always)@_transparent
-    public init(r: FloatLiteralType, πr: FloatLiteralType) {
-        self.init(rawValue: complex_new_rp(r, πr))
-    }
-    @inlinable@inline(__always)@_transparent
-    public var θ: FloatLiteralType {
-        complex_arg(rawValue);
-    }
-    @inlinable@inline(__always)@_transparent
-    public var conj: Self {
-        unsafeBitCast(complex_conj(rawValue), to: Self.self)
-    }
-    @inlinable@inline(__always)@_transparent
-    public var proj: Self {
-        unsafeBitCast(complex_proj(rawValue), to: Self.self)
-    }
 }
 //public typealias Complex64 = DSPComplex
 extension DSPComplex {//}: ComplexNumber, @unchecked Sendable {
