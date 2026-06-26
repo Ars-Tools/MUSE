653773550fc5a97011057c084bb74ea95e4735b9
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-26 15:21:57 +0900

    add lacgv

---
+++ AMX/Sources/lapack+misc.h
@@ -0,0 +1,15 @@
+//
+//  lapack+misc.h
+//  MUSE
+//
+//  Created by Kota on 6/26/26.
+//
+#include"module.h"
+void lacgv(__LAPACK_int const length,
+           complex64_t*__nonnull const x, __LAPACK_int const incx) {
+    zlacgv_(&length, x, incx);
+}
+void lacgv(__LAPACK_int const length,
+           complex128_t*__nonnull const x, __LAPACK_int const incx) {
+    zlacgv_(&length, x, incx);
+}
