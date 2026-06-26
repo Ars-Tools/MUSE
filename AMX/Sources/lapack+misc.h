da4e9f198ac40870d2ddc78ec9a17163ace0975e
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-26 15:29:31 +0900

    fix

--- AMX/Sources/lapack+misc.h
+++ AMX/Sources/lapack+misc.h
@@ -7,9 +7,9 @@
 #include"module.h"
 void lacgv(__LAPACK_int const length,
            complex64_t*__nonnull const x, __LAPACK_int const incx) {
-    zlacgv_(&length, x, incx);
+    clacgv_(&length, x, &incx);
 }
 void lacgv(__LAPACK_int const length,
            complex128_t*__nonnull const x, __LAPACK_int const incx) {
-    zlacgv_(&length, x, incx);
+    zlacgv_(&length, x, &incx);
 }
