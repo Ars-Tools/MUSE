b09840f6adcb4ee45e100e0498bb06eaf07ff666
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-06-26 15:30:14 +0900

    __attribute__((always_inline, __overloadable__, warn_unused_result)) inline static

--- AMX/Sources/lapack+misc.h
+++ AMX/Sources/lapack+misc.h
@@ -5,10 +5,12 @@
 //  Created by Kota on 6/26/26.
 //
 #include"module.h"
+__attribute__((always_inline, __overloadable__, warn_unused_result)) inline static
 void lacgv(__LAPACK_int const length,
            complex64_t*__nonnull const x, __LAPACK_int const incx) {
     clacgv_(&length, x, &incx);
 }
+__attribute__((always_inline, __overloadable__, warn_unused_result)) inline static
 void lacgv(__LAPACK_int const length,
            complex128_t*__nonnull const x, __LAPACK_int const incx) {
     zlacgv_(&length, x, &incx);
