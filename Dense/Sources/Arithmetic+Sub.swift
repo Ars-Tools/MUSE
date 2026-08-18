b6706c997702e379a306a557b743780176b3a23c
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-18 19:25:20 +0900

    Dense: update arithmetic sub

--- Dense/Sources/Arithmetic+Sub.swift
+++ Dense/Sources/Arithmetic+Sub.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Kota on 9/27/25.
 //
+import AltVec
 extension Arithmetic {
     public struct Sub<X: Tensor<Element>, Y: Tensor<Element>> {
         public typealias S = Sub<X.S, Y.S>
