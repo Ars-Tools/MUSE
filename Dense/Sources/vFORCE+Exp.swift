151dd53d25c2b16cdc47a487c6c7e671406175cd
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-18 19:25:47 +0900

    Dense: update vforce exp

--- Dense/Sources/vFORCE+Exp.swift
+++ Dense/Sources/vFORCE+Exp.swift
@@ -5,6 +5,7 @@
 //  Created by Kota on 9/27/25.
 //
 import func Layout.capacity
+import AltVec
 extension vFORCE {
     @frozen public struct Exp<X: Tensor<Element>> {
         public typealias S = Exp<X.S>
