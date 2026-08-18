817c638f3ccc4807ec9be18686fbc5e5505240d9
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-18 19:21:21 +0900

    AMX: rename altvec geometry to mkl geometry

--- AMX/Sources/mkl+geometry.h
+++ AMX/Sources/mkl+geometry.h
@@ -1,38 +1,76 @@
 //
-//  vDSP+geometry.h
+//  mkl+geometry.h
 //  MUSE
 //
 //  Created by Kota on 6/19/26.
 //
 #include"module.h"
-__attribute__((always_inline, __overloadable__)) static inline
+__attribute__((always_inline, overloadable)) static inline
 void vDSP_abs(float32_t const*__nonnull const x, intptr_t const incx,
               float32_t      *__nonnull const y, intptr_t const incy,
               intptr_t const length) {
     vDSP_vabs(x, incx, y, incy, length);
 }
-__attribute__((always_inline, __overloadable__)) static inline
+__attribute__((always_inline, overloadable)) static inline
 void vDSP_abs(float64_t const*__nonnull const x, intptr_t const incx,
               float64_t      *__nonnull const y, intptr_t const incy,
               intptr_t const length) {
     vDSP_vabsD(x, incx, y, incy, length);
 }
+// MARK: zabs
+__attribute__((always_inline, overloadable)) static inline
+void vDSP_abs(complex64_t const*__nonnull const x, intptr_t const incx,
+              float32_t      *__nonnull const y, intptr_t const incy,
+              intptr_t const length) {
+    vDSP_zvabs(&(DSPSplitComplex const) {
+        .realp = &x->r,
+        .imagp = &x->i
+    }, 2 * incx, y, incy, length);
+}
+__attribute__((always_inline, overloadable)) static inline
+void vDSP_abs(complex128_t const*__nonnull const x, intptr_t const incx,
+              float64_t      *__nonnull const y, intptr_t const incy,
+              intptr_t const length) {
+    vDSP_zvabsD(&(DSPDoubleSplitComplex const) {
+        .realp = &x->r,
+        .imagp = &x->i
+    }, 2 * incx, y, incy, length);
+}
+// MARK: zmags
+__attribute__((always_inline, overloadable)) static inline
+void vDSP_mags(complex64_t const*__nonnull const x, intptr_t const incx,
+               float32_t      *__nonnull const y, intptr_t const incy,
+               intptr_t const length) {
+    vDSP_zvmags(&(DSPSplitComplex const) {
+        .realp = &x->r,
+        .imagp = &x->i
+    }, 2 * incx, y, incy, length);
+}
+__attribute__((always_inline, overloadable)) static inline
+void vDSP_mags(complex128_t const*__nonnull const x, intptr_t const incx,
+               float64_t      *__nonnull const y, intptr_t const incy,
+               intptr_t const length) {
+    vDSP_zvmagsD(&(DSPDoubleSplitComplex const) {
+        .realp = &x->r,
+        .imagp = &x->i
+    }, 2 * incx, y, incy, length);
+}
 // MARK: hypot
-__attribute__((always_inline, __overloadable__)) static inline
+__attribute__((always_inline, overloadable)) static inline
 void vDSP_hypot(float32_t const*__nonnull const x, intptr_t const incx,
                 float32_t const*__nonnull const y, intptr_t const incy,
                 float32_t      *__nonnull const z, intptr_t const incz,
                 intptr_t const length) {
     vDSP_vdist(x, incx, y, incy, z, incz, length);
 }
-__attribute__((always_inline, __overloadable__)) static inline
+__attribute__((always_inline, overloadable)) static inline
 void vDSP_hypot(float64_t const*__nonnull const x, intptr_t const incx,
                 float64_t const*__nonnull const y, intptr_t const incy,
                 float64_t      *__nonnull const z, intptr_t const incz,
                 intptr_t const length) {
     vDSP_vdistD(x, incx, y, incy, z, incz, length);
 }
-__attribute__((always_inline, __overloadable__)) static inline
+__attribute__((always_inline, overloadable)) static inline
 void vDSP_hypot_zsq(float32_t const*__nonnull const x,
                     float32_t const*__nonnull const y,
                     intptr_t const incz,
@@ -43,7 +81,7 @@ void vDSP_hypot_zsq(float32_t const*__nonnull const x,
         .imagp = y
     }, incz, w, incw, length);
 }
-__attribute__((always_inline, __overloadable__)) static inline
+__attribute__((always_inline, overloadable)) static inline
 void vDSP_hypot_zsq(float64_t const*__nonnull const x,
                     float64_t const*__nonnull const y,
                     intptr_t const incz,
