821072c1c6fdb5be5f4b2b21862d6647264dd5c1
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-18 19:25:47 +0900

    Dense: update vforce arc

--- Dense/Sources/vFORCE+Arc.swift
+++ Dense/Sources/vFORCE+Arc.swift
@@ -6,6 +6,7 @@
 //
 import typealias Layout.MemoryStrategy
 import func Layout.capacity
+import AltVec
 extension vFORCE {
     @frozen public struct ArcSin<X: Tensor<Element>> {
         public typealias S = vFORCE.ArcSin<X.S>
