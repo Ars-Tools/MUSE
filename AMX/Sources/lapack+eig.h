3c4f46d02ae26bca15bf39d1097b74abb09b0c52
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-28 19:10:00 +0900

    simplified

--- AMX/Sources/lapack+eig.h
+++ AMX/Sources/lapack+eig.h
@@ -14,32 +14,17 @@ __LAPACK_int const geev(__LAPACK_int const n,
                         float32_t*__nullable const vl, __LAPACK_int const ldvl,
                         float32_t*__nullable const vr, __LAPACK_int const ldvr,
                         float32_t*__nullable const work, __LAPACK_int const lwork) {
-    char const jobvl = vl ? 'V' : 'N';
-    char const jobvr = vr ? 'V' : 'N';
-    if ( work ) {
-        __LAPACK_int info;
-        sgeev_(&jobvl, &jobvr,
-               &n,
-               A, &ldA,
-               r,
-               i,
-               vl, &ldvl,
-               vr, &ldvr,
-               work, &lwork, &info);
-        return info;
-    } else {
-        __LAPACK_int info;
-        float32_t size;
-        sgeev_(&jobvl, &jobvr,
-               &n,
-               A, &ldA,
-               r,
-               i,
-               vl, &ldvl,
-               vr, &ldvr,
-               &size, (__LAPACK_int const[]){-1}, &info);
-        return info ? info : size;
-    }
+    __LAPACK_int info;
+    float32_t size;
+    sgeev_((char const*__nonnull const)"NV" + !!vl, (char const*__nonnull const)"NV" + !!vr,
+           &n,
+           A, &ldA,
+           r, i,
+           vl, &ldvl,
+           vr, &ldvr,
+           work ? work : &size, work ? &lwork : (__LAPACK_int const[]) {-1},
+           &info);
+    return info ? info : work ? info : size;
 }
 __attribute__((always_inline, __overloadable__, warn_unused_result)) inline static
 __LAPACK_int const geev(__LAPACK_int const n,
@@ -49,33 +34,17 @@ __LAPACK_int const geev(__LAPACK_int const n,
                         float64_t*__nullable const vl, __LAPACK_int const ldvl,
                         float64_t*__nullable const vr, __LAPACK_int const ldvr,
                         float64_t*__nullable const work, __LAPACK_int const lwork) {
-    char const jobvl = vl ? 'V' : 'N';
-    char const jobvr = vr ? 'V' : 'N';
-    if ( work ) {
-        __LAPACK_int info;
-        dgeev_(&jobvl,
-               &jobvr,
-               &n,
-               A, &ldA,
-               r,
-               i,
-               vl, &ldvl,
-               vr, &ldvr,
-               work, &lwork, &info);
-        return info;
-    } else {
-        __LAPACK_int info;
-        float64_t size;
-        dgeev_(&jobvl, &jobvr,
-               &n,
-               A, &ldA,
-               r,
-               i,
-               vl, &ldvl,
-               vr, &ldvr,
-               &size, (__LAPACK_int const[]){-1}, &info);
-        return info ? info : size;
-    }
+    __LAPACK_int info;
+    float64_t size;
+    dgeev_((char const*__nonnull const)"NV" + !!vl, (char const*__nonnull const)"NV" + !!vr,
+           &n,
+           A, &ldA,
+           r, i,
+           vl, &ldvl,
+           vr, &ldvr,
+           work ? work : &size, work ? &lwork : (__LAPACK_int const[]) {-1},
+           &info);
+    return info ? info : work ? info : size;
 }
 __attribute__((always_inline, __overloadable__, warn_unused_result)) inline static
 __LAPACK_int const geev(__LAPACK_int const n,
@@ -85,32 +54,17 @@ __LAPACK_int const geev(__LAPACK_int const n,
                         complex64_t*__nullable const vr, __LAPACK_int const ldvr,
                         complex64_t*__nullable const work, __LAPACK_int const lwork,
                         float32_t*__nonnull const rwork /* require 2N elements space */) {
-    char const jobvl = vl ? 'V' : 'N';
-    char const jobvr = vr ? 'V' : 'N';
-    if ( work ) {
-        __LAPACK_int info;
-        cgeev_(&jobvl, &jobvr,
-               &n,
-               A, &ldA,
-               w,
-               vl, &ldvl,
-               vr, &ldvr,
-               work, &lwork, rwork,
-               &info);
-        return info;
-    } else {
-        __LAPACK_int info;
-        __complex float size;
-        cgeev_(&jobvl, &jobvr,
-               &n,
-               A, &ldA,
-               w,
-               vl, &ldvl,
-               vr, &ldvr,
-               &size, (__LAPACK_int const[]){-1}, rwork,
-               &info);
-        return info ? info : size;
-    }
+    __LAPACK_int info;
+    __complex float size;
+    cgeev_((char const*__nonnull const)"NV" + !!vl, (char const*__nonnull const)"NV" + !!vr,
+           &n,
+           A, &ldA,
+           w,
+           vl, &ldvl,
+           vr, &ldvr,
+           work ? work : &size, work ? &lwork : (__LAPACK_int const[]) {-1}, rwork,
+           &info);
+    return info ? info : work ? info : size;
 }
 __attribute__((always_inline, __overloadable__, warn_unused_result)) inline static
 __LAPACK_int const geev(__LAPACK_int const n,
@@ -120,30 +74,15 @@ __LAPACK_int const geev(__LAPACK_int const n,
                         complex128_t*__nullable const vr, __LAPACK_int const ldvr,
                         complex128_t*__nullable const work, __LAPACK_int const lwork,
                         float64_t*__nonnull const rwork /* require 2N elements space */) {
-    char const jobvl = vl ? 'V' : 'N';
-    char const jobvr = vr ? 'V' : 'N';
-    if ( work ) {
-        __LAPACK_int info;
-        zgeev_(&jobvl, &jobvr,
-               &n,
-               A, &ldA,
-               w,
-               vl, &ldvl,
-               vr, &ldvr,
-               work, &lwork, rwork,
-               &info);
-        return info;
-    } else {
-        __LAPACK_int info;
-        __complex double size;
-        zgeev_(&jobvl, &jobvr,
-               &n,
-               A, &ldA,
-               w,
-               vl, &ldvl,
-               vr, &ldvr,
-               &size, (__LAPACK_int const[]){-1}, rwork,
-               &info);
-        return info ? info : size;
-    }
+    __LAPACK_int info;
+    __complex double size;
+    zgeev_((char const*__nonnull const)"NV" + !!vl, (char const*__nonnull const)"NV" + !!vr,
+           &n,
+           A, &ldA,
+           w,
+           vl, &ldvl,
+           vr, &ldvr,
+           work ? work : &size, work ? &lwork : (__LAPACK_int const[]) {-1}, rwork,
+           &info);
+    return info ? info : work ? info : size;
 }
